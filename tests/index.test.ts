5280ceadf13e1f2f9c9e93cccaf7e75c6987e14b
mounibec <mounibec@gmail.com>
2019-12-31 12:39:04 +0100

    Expecting failure when build tx if balance is not enough

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,6 +1,6 @@
 import * as dotenv from "dotenv";
 
-import {ParaSwap, OptimalRates, Token, Transaction} from "../src";
+import {ParaSwap, OptimalRates, Token, Transaction, APIError} from "../src";
 import BigNumber from "bignumber.js";
 
 dotenv.config();
@@ -15,6 +15,7 @@ const srcToken = '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee';
 const destToken = '0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359';
 const srcAmount = '1000000000000000000'; //The source amount multiplied by its decimals
 const senderAddress = '0xfceA770875E7e6f25E33CEa5188d12Ef234606b4';
+const referrer = 'sdk-test';
 const payTo = '0x8B4e846c90a2521F0D2733EaCb56760209EAd51A'; // Useful in case of a payment
 
 describe("ParaSwap SDL", () => {
@@ -49,8 +50,8 @@ describe("ParaSwap SDL", () => {
 
     expect(Array.isArray(bestRoute)).toBe(true);
 
-    expect(typeof bestRoute[0].destAmount).toBe('string');
-    expect(new BigNumber(bestRoute[0].destAmount).isNaN()).toBe(false);
+    expect(typeof bestRoute[0].amount).toBe('string');
+    expect(new BigNumber(bestRoute[0].amount).isNaN()).toBe(false);
 
     expect(typeof bestRoute[0].exchange).toBe('string');
 
@@ -62,13 +63,13 @@ describe("ParaSwap SDL", () => {
 
     expect(Array.isArray(others)).toBe(true);
 
-    expect(typeof others[0].exchange).toBe('string');
+    expect(typeof others![0].exchange).toBe('string');
 
-    expect(typeof others[0].rate).toBe('string');
-    expect(new BigNumber(others[0].rate).isNaN()).toBe(false);
+    expect(typeof others![0].rate).toBe('string');
+    expect(new BigNumber(others![0].rate).isNaN()).toBe(false);
 
-    expect(typeof others[0].unit).toBe('string');
-    expect(new BigNumber(others[0].unit).isNaN()).toBe(false);
+    expect(typeof others![0].unit).toBe('string');
+    expect(new BigNumber(others![0].unit).isNaN()).toBe(false);
   });
 
   test("Get_Build_Tx", async () => {
@@ -76,14 +77,12 @@ describe("ParaSwap SDL", () => {
     const priceRoute = ratesOrError as OptimalRates;
 
     const destAmount = priceRoute.amount;
-    const txOrError = await paraSwap.buildTx(srcToken, destToken, srcAmount, destAmount, priceRoute, senderAddress, payTo);
+    const txOrError = await paraSwap.buildTx(srcToken, destToken, srcAmount, destAmount, priceRoute, senderAddress, referrer, payTo);
 
-    const transaction = txOrError as Transaction;
+    const error = txOrError as APIError;
 
-    expect(typeof transaction.from).toBe('string');
-    expect(typeof transaction.to).toBe('string');
-    expect(typeof transaction.value).toBe('string');
-    expect(typeof transaction.data).toBe('string');
-    expect(typeof transaction.chainId).toBe('number');
+    expect(typeof error.status).toBe("number");
+    expect(error.status).toBe(403);
+    expect(error.message).toBe('ETH_BALANCE_ERROR: Not Enough ETH Balance');
   });
 });
