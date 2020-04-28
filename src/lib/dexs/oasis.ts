5bc4a085d4c4032d7c4909fc2fb0c9dd888979d2
mounibec <mounibec@gmail.com>
2020-04-28 02:48:35 +0200

    Refactoring Oasis

---
+++ src/lib/dexs/oasis.ts
@@ -0,0 +1,13 @@
+const DEX_PARAMS: any = {
+  1: {
+    otc: "0x794e6e91555438aFc3ccF1c5076A74F42133d08D",
+    weth: "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
+    factory: "0x4678f0a6958e4d2bc4f1baf7bc52e8f3564f3fe4"
+  }
+};
+
+export class Oasis {
+  static getExchangeParams(network: number = 1) {
+    return DEX_PARAMS[network];
+  }
+}
