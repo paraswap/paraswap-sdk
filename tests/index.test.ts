c3c3e117fa6bb8c3fe1528dccf0173388639a1aa
mounibec <mounibec@gmail.com>
2020-05-26 18:28:18 +0200

    Tests clean ups

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -17,14 +17,13 @@ const BAT = "0x0d8775f648430679a709e98d2b0cb6250d2887ef";
 const MANA = "0x0f5d2fb29fb7d3cfee444a200298f468908cc942";
 
 const PROVIDER_URL = process.env.PROVIDER_URL;
-const apiURL = process.env.API_URL || 'https://paraswap.io/api';
 const network = 1;
 const srcToken = ETH;
 const destToken = DAI;
 const srcAmount = (10 * 1e18).toString(); //The source amount multiplied by its decimals
 const senderAddress = '0xfceA770875E7e6f25E33CEa5188d12Ef234606b4';
 const referrer = 'sdk-test';
-const receiver = '0x8B4e846c90a2521F0D2733EaCb56760209EAd51A'; // Useful in case of a payment
+const receiver = '0x8B4e846c90a2521F0D2733EaCb56760209EAd51A';
 
 const provider = new Web3(new Web3.providers.HttpProvider(PROVIDER_URL));
 
@@ -32,7 +31,7 @@ describe("ParaSwap SDK", () => {
   let paraSwap: ParaSwap;
 
   beforeAll(async () => {
-    paraSwap = new ParaSwap(network, apiURL).setWeb3Provider(provider);
+    paraSwap = new ParaSwap(network).setWeb3Provider(provider);
 
     paraSwap.adapters = (await paraSwap.getAdapters()) as Adapters;
   });
@@ -52,7 +51,7 @@ describe("ParaSwap SDK", () => {
   });
 
   test("Get_Rates", async () => {
-    const ratesOrError = await paraSwap.getRate(srcToken, destToken, srcAmount, {excludeDEXS: "Uniswap"});
+    const ratesOrError = await paraSwap.getRate("ETH", "DAI", srcAmount, {excludeDEXS: "Uniswap"});
 
     const priceRoute = ratesOrError as OptimalRates;
 
@@ -120,7 +119,7 @@ describe("ParaSwap SDK", () => {
     const srcToken = new Token(ETH, 18, 'ETH');
     const destToken = new Token(DAI, 18, 'DAI');
 
-    const ratesOrError = await paraSwap.getRate(srcToken.address, destToken.address, srcAmount, {includeDEXS: 'Uniswap'});
+    const ratesOrError = await paraSwap.getRate(srcToken.symbol, destToken.symbol, srcAmount, {includeDEXS: 'Uniswap'});
     const priceRoute = ratesOrError as OptimalRates;
 
     const destAmount = priceRoute.amount;
