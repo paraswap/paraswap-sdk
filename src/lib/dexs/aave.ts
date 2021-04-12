67ece28883b43efb8222b580f35cc390ab44c15d
Shresth Agrawal <shresthagrawal.31@gmail.com>
2021-04-12 14:14:08 +0200

    Add support for Aave for Polygon
    Aave weth bridge interface was slightly modified for the
    polygon deployment.

--- src/lib/dexs/aave.ts
+++ src/lib/dexs/aave.ts
@@ -4,7 +4,8 @@ import { AaveDexData, DexParams } from './dex-types';
 import ERC20_ABI = require('../../abi/erc20.json');
 import AAVE_LENDING_POOL_ABI_V1 = require('../../abi/aave-lending-pool.v1.json');
 import AAVE_LENDING_POOL_ABI_V2 = require('../../abi/aave-lending-pool.v2.json');
-import WETH_GATEWAY_ABI = require('../../abi/aave-weth-gateway.json');
+import WETH_GATEWAY_ABI_1 = require('../../abi/aave-weth-gateway.json');
+import WETH_GATEWAY_ABI_137 = require('../../abi/aave-weth-gateway-137.json');
 
 const aaveLendingPool: any = {
   v1: {
@@ -12,6 +13,7 @@ const aaveLendingPool: any = {
   },
   v2: {
     1: '0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9',
+    137: '0x8dff5e27ea6b7ac08ebfdf9eb090f32ee9a30fcf',
   },
 };
 
@@ -21,6 +23,12 @@ const SPENDER: any = {
 
 const WETH_GATEWAY: any = {
   1: '0xDcD33426BA191383f1c9B431A342498fdac73488',
+  137: '0xbEadf48d62aCC944a06EEaE0A9054A90E5A7dc97',
+};
+
+const WETH_GATEWAY_ABI: any = {
+  1: WETH_GATEWAY_ABI_1,
+  137: WETH_GATEWAY_ABI_137,
 };
 
 const REF_CODE = 1;
@@ -36,12 +44,30 @@ export class Aave extends Adapter {
     };
   }
 
-  private async _swap(srcToken: Address, destToken: Address, data: AaveDexData): Promise<DexParams> {
+  private async _swap(
+    srcToken: Address,
+    destToken: Address,
+    data: AaveDexData,
+  ): Promise<DexParams> {
     if (data.isV2) {
-
       if (this.isETHAddress(srcToken)) {
-        const wethGateway = new this.web3Provider.eth.Contract(WETH_GATEWAY_ABI, WETH_GATEWAY[this.network]);
-        const swapData = wethGateway.methods.depositETH(this.augustus._address, REF_CODE).encodeABI();
+        // The interface for WETH_GATEWAY was slightly updated in the polygon release
+        const wethGateway = new this.web3Provider.eth.Contract(
+          WETH_GATEWAY_ABI[this.network],
+          WETH_GATEWAY[this.network],
+        );
+        const swapData =
+          this.network === 1
+            ? wethGateway.methods
+                .depositETH(this.augustus._address, REF_CODE)
+                .encodeABI()
+            : wethGateway.methods
+                .depositETH(
+                  aaveLendingPool.v2[this.network],
+                  this.augustus._address,
+                  REF_CODE,
+                )
+                .encodeABI();
 
         return super.swap(
           srcToken,
@@ -50,10 +76,24 @@ export class Aave extends Adapter {
           swapData,
           WETH_GATEWAY[this.network],
         );
-
       } else if (this.isETHAddress(destToken)) {
-        const wethGateway = new this.web3Provider.eth.Contract(WETH_GATEWAY_ABI, WETH_GATEWAY[this.network]);
-        const swapData = wethGateway.methods.withdrawETH(data.srcAmount, this.augustus._address).encodeABI();
+        // The interface for WETH_GATEWAY was slightly updated in the polygon release
+        const wethGateway = new this.web3Provider.eth.Contract(
+          WETH_GATEWAY_ABI[this.network],
+          WETH_GATEWAY[this.network],
+        );
+        const swapData =
+          this.network === 1
+            ? wethGateway.methods
+                .withdrawETH(data.srcAmount, this.augustus._address)
+                .encodeABI()
+            : wethGateway.methods
+                .withdrawETH(
+                  aaveLendingPool.v2[this.network],
+                  data.srcAmount,
+                  this.augustus._address,
+                )
+                .encodeABI();
 
         return super.swap(
           srcToken,
@@ -62,11 +102,15 @@ export class Aave extends Adapter {
           swapData,
           WETH_GATEWAY[this.network],
         );
-
       } else {
         if (data.fromAToken) {
-          const lpContract = new this.web3Provider.eth.Contract(AAVE_LENDING_POOL_ABI_V2, aaveLendingPool.v2[this.network]);
-          const swapData = lpContract.methods.withdraw(destToken, data.srcAmount, this.augustus._address).encodeABI();
+          const lpContract = new this.web3Provider.eth.Contract(
+            AAVE_LENDING_POOL_ABI_V2,
+            aaveLendingPool.v2[this.network],
+          );
+          const swapData = lpContract.methods
+            .withdraw(destToken, data.srcAmount, this.augustus._address)
+            .encodeABI();
 
           return super.swap(
             srcToken,
@@ -75,10 +119,14 @@ export class Aave extends Adapter {
             swapData,
             aaveLendingPool.v2[this.network],
           );
-
         } else {
-          const lpContract = new this.web3Provider.eth.Contract(AAVE_LENDING_POOL_ABI_V2, aaveLendingPool.v2[this.network]);
-          const swapData = lpContract.methods.deposit(srcToken, data.srcAmount, this.augustus._address, REF_CODE).encodeABI();
+          const lpContract = new this.web3Provider.eth.Contract(
+            AAVE_LENDING_POOL_ABI_V2,
+            aaveLendingPool.v2[this.network],
+          );
+          const swapData = lpContract.methods
+            .deposit(srcToken, data.srcAmount, this.augustus._address, REF_CODE)
+            .encodeABI();
 
           return super.swap(
             srcToken,
@@ -91,27 +139,36 @@ export class Aave extends Adapter {
       }
     } else {
       if (data.fromAToken) {
-        const aContract = new this.web3Provider.eth.Contract(ERC20_ABI, srcToken);
-        const swapData = aContract.methods.redeem(data.srcAmount).encodeABI();
-
-        return super.swap(
-          srcToken,
-          destToken,
-          data,
-          swapData,
+        const aContract = new this.web3Provider.eth.Contract(
+          ERC20_ABI,
           srcToken,
         );
+        const swapData = aContract.methods.redeem(data.srcAmount).encodeABI();
 
+        return super.swap(srcToken, destToken, data, swapData, srcToken);
       } else {
-        const approveCallData = await this.getApproveCallData(srcToken, data.srcAmount, SPENDER[this.network]);
+        const approveCallData = await this.getApproveCallData(
+          srcToken,
+          data.srcAmount,
+          SPENDER[this.network],
+        );
 
-        const lpContract = new this.web3Provider.eth.Contract(AAVE_LENDING_POOL_ABI_V1, aaveLendingPool.v1[this.network]);
-        const swapData = lpContract.methods.deposit(srcToken, data.srcAmount, REF_CODE).encodeABI();
+        const lpContract = new this.web3Provider.eth.Contract(
+          AAVE_LENDING_POOL_ABI_V1,
+          aaveLendingPool.v1[this.network],
+        );
+        const swapData = lpContract.methods
+          .deposit(srcToken, data.srcAmount, REF_CODE)
+          .encodeABI();
 
         const mintValue = this.isETHAddress(srcToken) ? data.srcAmount : '0';
 
-        const callees = approveCallData ? [this.augustus._address, aaveLendingPool.v1[this.network]] : [aaveLendingPool.v1[this.network]];
-        const calldata = approveCallData ? [approveCallData.calldata, swapData] : [swapData];
+        const callees = approveCallData
+          ? [this.augustus._address, aaveLendingPool.v1[this.network]]
+          : [aaveLendingPool.v1[this.network]];
+        const calldata = approveCallData
+          ? [approveCallData.calldata, swapData]
+          : [swapData];
         const values = approveCallData ? ['0', mintValue] : [mintValue];
 
         return {
@@ -123,7 +180,11 @@ export class Aave extends Adapter {
     }
   }
 
-  async buildSwap(srcToken: Address, destToken: Address, data: AaveDexData): Promise<DexParams> {
+  async buildSwap(
+    srcToken: Address,
+    destToken: Address,
+    data: AaveDexData,
+  ): Promise<DexParams> {
     return this._swap(srcToken, destToken, data);
   }
 }
