848db19dd8deed61f2c1fa414fc6893d28f0e4bf
Velenir <velenir@gmail.com>
2022-03-09 14:11:00 +0300

    add examples (temp)

---
+++ src/examples/sdk.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+import { ethers } from "ethers";
+import { constructSDK } from "..";
+import { constructContractCaller, constructFetcher } from "../helpers";
+
+const fetcher = constructFetcher(axios)
+
+const provider = ethers.getDefaultProvider(1)
+const contractCaller = constructContractCaller(provider)
+
+const paraswap = constructSDK({
+  apiURL: "",
+  network: 1,
+  fetcher,
+  contractCaller
+})
+
+const res = paraswap.getAdapters({type: "list", namesOnly: false})
