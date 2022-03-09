f2832b8bc95e9e74dd46bc7b0c524b82f1bc982f
Velenir <velenir@gmail.com>
2022-03-09 14:11:54 +0300

    constructGetAdapters type improv

--- src/adapters.ts
+++ src/adapters.ts
@@ -2,35 +2,43 @@ import { ConstructFetchInput } from "./types"
 import { constructSearchString } from "./helpers/misc"
 
 type Adapter = {
-    adapter: string;
-    index: number;
+  adapter: string;
+  index: number;
 }
 
-type AdaptersAsObject  = Record<string, Adapter[]>
-type AdaptersAsList = {name: string, adapters: Adapter[]}[]
+type AdaptersAsObject = Record<string, Adapter[]>
+type AdaptersAsList = { name: string, adapters: Adapter[] }[]
 type AdaptersAsStrings = string[]
-type AllAdaptersOptions =  { type: 'object' } | {type: 'list', namesOnly?: false} |{type: 'list', namesOnly: true}
+
+type OptionsObject = { type: 'object' }
+type OptionsList = { type: 'list'; namesOnly?: false }
+type OptionsListNamesOnly = { type: 'list'; namesOnly: true }
+
+type AllAdaptersOptions = OptionsObject | OptionsList | OptionsListNamesOnly
+
+interface GetAdapternsFunc {
+  (options: OptionsObject): Promise<AdaptersAsObject>
+  (options: OptionsList): Promise<AdaptersAsList>
+  (options: OptionsListNamesOnly): Promise<AdaptersAsStrings>
+}
 
 export type ConstructAdaptersFunctions = {
-   getAdapters: 
-        | ((options: { type: 'object' }) => Promise<AdaptersAsObject>) 
-        | ((options: { type: 'list'; namesOnly: false }) => Promise<AdaptersAsList>)
-        | ((options: { type: 'list'; namesOnly: true }) => Promise<AdaptersAsStrings>)
+  getAdapters: GetAdapternsFunc
 }
 
 export const constructGetAdapters = (
-    { apiURL, network, fetcher}: ConstructFetchInput
+  { apiURL, network, fetcher }: ConstructFetchInput
 ): ConstructAdaptersFunctions => {
-    async function getAdapters(options: {type: 'object'}) : Promise<AdaptersAsObject>
-    async function getAdapters(options: {type: 'list', namesOnly: false}):  Promise<AdaptersAsList>
-    async function getAdapters(options: {type: 'list', namesOnly: true}): Promise<AdaptersAsStrings>
-    async function getAdapters(options: AllAdaptersOptions) : Promise<AdaptersAsObject | AdaptersAsList | AdaptersAsStrings> {
-      const query = constructSearchString({
-        network,
-        namesOnly: !!options && 'namesOnly' in options ? options.namesOnly : undefined
-     })
-    const fetchURL = `${apiURL}/adapters${options?.type === 'list' ? '/list':''}${query}`
- 
+  async function getAdapters(options: OptionsObject): Promise<AdaptersAsObject>
+  async function getAdapters(options: OptionsList): Promise<AdaptersAsList>
+  async function getAdapters(options: OptionsListNamesOnly): Promise<AdaptersAsStrings>
+  async function getAdapters(options: AllAdaptersOptions): Promise<AdaptersAsObject | AdaptersAsList | AdaptersAsStrings> {
+    const query = constructSearchString({
+      network,
+      namesOnly: !!options && 'namesOnly' in options ? options.namesOnly : undefined
+    })
+    const fetchURL = `${apiURL}/adapters${options?.type === 'list' ? '/list' : ''}${query}`
+
     const data = await fetcher<AdaptersAsObject | AdaptersAsList | AdaptersAsStrings>({
       url: fetchURL,
       method: "GET",
