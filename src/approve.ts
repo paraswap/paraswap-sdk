ea739244462c785101a4476ea85545ede710a5f5
Velenir <velenir@gmail.com>
2022-03-15 13:41:34 +0300

    include min abi necessary

--- src/approve.ts
+++ src/approve.ts
@@ -20,6 +20,27 @@ export type ApproveTokenFunctions = {
   approveTokenBulk: ApproveTokenBulk;
 };
 
+// much smaller than the whole ERC20_ABI
+const MinERC20Abi = [
+  {
+    constant: false,
+    inputs: [
+      { name: '_spender', type: 'address' },
+      { name: '_value', type: 'uint256' },
+    ],
+    name: 'approve',
+    outputs: [{ name: '', type: 'bool' }],
+    payable: false,
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+] as const;
+
+type ExtractAbiMethodNames<T extends readonly { name: string }[]> =
+  T[number]['name'];
+
+type AvailableMethods = ExtractAbiMethodNames<typeof MinERC20Abi>;
+
 // @TODO make this generic to return whatever `contractCaller` returns
 // to allow for better versatility
 export const constructApproveToken = (
@@ -38,11 +59,9 @@ export const constructApproveToken = (
   ) => {
     const spender = _spender || (_spender = await getSpender(signal));
 
-    const { default: ERC20_ABI } = await import('./abi/ERC20.json');
-
-    const res = await options.contractCaller<TxHash>({
+    const res = await options.contractCaller<TxSentResult, AvailableMethods>({
       address: tokenAddress,
-      abi: ERC20_ABI,
+      abi: MinERC20Abi,
       contractMethod: 'approve',
       args: [spender, amount],
       overrides,
