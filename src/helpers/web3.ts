74da5e42d25350213ab9da758423e87333d4fbc9
Velenir <velenir@gmail.com>
2022-03-15 19:08:00 +0300

    helpers/web3 returns UnpromiEvent

--- src/helpers/web3.ts
+++ src/helpers/web3.ts
@@ -1,19 +1,28 @@
-import type { Address, ContractCallerFunction } from '../types';
+import type {
+  Address,
+  ContractCallerFunctions,
+  StaticContractCallerFn,
+  TransactionContractCallerFn,
+} from '../types';
 import type Web3 from 'web3';
 import type { AbiItem } from 'web3-utils';
 import type {
   ContractSendMethod,
   SendOptions,
   CallOptions,
+  Contract,
 } from 'web3-eth-contract';
+import type { PromiEvent } from 'web3-core';
 import { assert } from 'ts-essentials';
 import { assertContractHasMethods } from './misc';
+
+export type UnpromiEvent = Pick<PromiEvent<Contract>, 'on' | 'once'>;
 
 export const constructContractCaller = (
   web3: Web3,
   account?: Address
-): ContractCallerFunction => {
-  const contractCallerFunction: ContractCallerFunction = async (params) => {
+): ContractCallerFunctions<UnpromiEvent> => {
+  const staticCall: StaticContractCallerFn = async (params) => {
     assert(web3.currentProvider, 'web3.currentProvider is not set');
 
     if (params.static) {
@@ -37,6 +46,12 @@ export const constructContractCaller = (
         normalizedOverrides
       );
     }
+  };
+
+  const transactCall: TransactionContractCallerFn<UnpromiEvent> = async (
+    params
+  ) => {
+    assert(web3.currentProvider, 'web3.currentProvider is not set');
 
     // assert(account, 'account must be specified to create a signer');
     // FIXME: how to assert properly if user passed signer
@@ -66,16 +81,20 @@ export const constructContractCaller = (
       ...args
     ) as ContractSendMethod;
 
-    return new Promise<string>((resolve, reject) => {
-      preparedCall.send(normalizedOverrides, (err, transactionHash) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(transactionHash);
-        }
-      });
-    });
+    const promiEvent = preparedCall.send(normalizedOverrides);
+
+    // can't just return promiEvent, because async function returns a Promise<PromiEvent>
+    // and await Promise<PromiEvent> automatically awaits the value of PromiEvent,
+    // that is await Promise<PromiEvent> = Awaited<PromiEvent> that doesn't have .on|once
+    // so that functionality becomes lost
+    // transactCall can be made sync, but approve has to be async to await getSpender()
+    const unpromiEvent: UnpromiEvent = {
+      on: promiEvent.on.bind(promiEvent),
+      once: promiEvent.once.bind(promiEvent),
+    };
+
+    return unpromiEvent;
   };
 
-  return contractCallerFunction;
+  return { staticCall, transactCall };
 };
