007a4f90d616e30f365888638c03cd331e1335f1
Velenir <velenir@gmail.com>
2022-04-22 15:48:52 +0300

    CancelLimitOrderFunctions<T> detection along with ApproveTokenFunctions<T> detection

--- src/sdk/partial.ts
+++ src/sdk/partial.ts
@@ -12,30 +12,53 @@ export type SDKConfig<TxResponse = any> = ConstructProviderFetchInput<
 > &
   ConstructFetchInput;
 
-type AnyFunction = (...args: any[]) => any;
-
 type SDKFunction<T extends ConstructBaseInput> = (
   config: T
-) => Record<string, AnyFunction>;
+) => Record<string, AnyFunction | Record<string, AnyFunction>>;
 
 type IntersectionOfReturns<Funcs extends AnyFunction[]> = UnionToIntersection<
   ReturnType<Funcs[number]>
 >;
 
 type ApproveTokenFunctionsKeys = keyof ApproveTokenFunctions<any>;
+type CancelLimitOrderFunctionsKeys = keyof CancelLimitOrderFunctions<any>;
+type TxResponseDetectKeys =
+  | ApproveTokenFunctionsKeys
+  | CancelLimitOrderFunctionsKeys;
+type TxResponseDetectFuncs =
+  | ApproveTokenFunctions<any>
+  | CancelLimitOrderFunctions<any>;
+
 
 type PartialSDKResult<
   Config extends ConstructBaseInput,
   Funcs extends [SDKFunction<Config>, ...SDKFunction<Config>[]]
-> = IntersectionOfReturns<Funcs> extends ApproveTokenFunctions<any> // if there are ApproveTokenFunctions in the intersection
-  ? // which means constructApproveToken was passed in Funcs
-    Omit<IntersectionOfReturns<Funcs>, ApproveTokenFunctionsKeys> &
-      ApproveTokenFunctions<
-        // infer what TxResponse was used in Config
-        Config extends SDKConfig<infer TxResponse> ? TxResponse : unknown
-        // and make the ApproveTokenFunctions<unknow> in the intersection a specific ApproveTokenFunctions<TxResponse>
-      >
-  : IntersectionOfReturns<Funcs>;
+> =
+  // if can infer TxResponse inside Config
+  Config extends SDKConfig<infer TxResponse>
+    ? // if there are ApproveTokenFunctions or CancelLimitOrderFunctions in the intersection
+      IntersectionOfReturns<Funcs> extends TxResponseDetectFuncs
+      ? // which means constructApproveToken or constructCancelLimitOrder was passed in Funcs
+        // then remove them
+        Omit<IntersectionOfReturns<Funcs>, TxResponseDetectKeys> &
+          // and add them again but with specific TxResponse
+          // if both were passed
+          (IntersectionOfReturns<Funcs> extends ApproveTokenFunctions<any> &
+            CancelLimitOrderFunctions<any>
+            ? // make the CancelLimitOrderFunctions<unknow> in the intersection a specific CancelLimitOrderFunctions<TxResponse>
+              CancelLimitOrderFunctions<TxResponse> &
+                // and make the ApproveTokenFunctions<unknow> in the intersection a specific ApproveTokenFunctions<TxResponse>
+                ApproveTokenFunctions<TxResponse>
+            : // if constructApproveToken was passed
+            IntersectionOfReturns<Funcs> extends ApproveTokenFunctions<any>
+            ? ApproveTokenFunctions<TxResponse>
+            : // if constructCancelLimitOrder was passed
+            IntersectionOfReturns<Funcs> extends CancelLimitOrderFunctions<any>
+            ? CancelLimitOrderFunctions<TxResponse>
+            : // and make the CancelLimitOrderFunctions<unknow> in the intersection a specific CancelLimitOrderFunctions<TxResponse>
+              IntersectionOfReturns<Funcs>)
+      : IntersectionOfReturns<Funcs>
+    : IntersectionOfReturns<Funcs>;
 
 /** @description construct composable SDK with methods you choose yourself */
 export const constructPartialSDK = <
