bcadc3f6fabb01bf02b2625cdc1e4a40d0693056
Velenir <velenir@gmail.com>
2022-04-26 13:35:11 +0300

    add compounded constructSubmitLimitOrder

--- src/methods/limitOrders/index.ts
+++ src/methods/limitOrders/index.ts
@@ -1,1 +1,48 @@
-export * from './buildOrderData';
+import type { ConstructProviderFetchInput } from '../../types';
+import type { LimitOrderToSend } from './helpers/types';
+import { BuildLimitOrderInput, constructBuildLimitOrder } from './buildOrder';
+import {
+  CancelLimitOrderFunctions,
+  constructCancelLimitOrder,
+} from './cancelOrder';
+import { constructGetLimitOrders, GetLimitOrdersFunctions } from './getOrders';
+import { constructPostLimitOrder, OpenLimitOrder } from './postOrder';
+import { constructSignLimitOrder } from './signOrder';
+
+type SubmitLimitOrder = (
+  buildLimitOrderParams: BuildLimitOrderInput,
+  signal?: AbortSignal
+) => Promise<OpenLimitOrder>;
+
+export type SubmitLimitOrderFuncs = {
+  submitLimitOrder: SubmitLimitOrder;
+};
+
+export const constructSubmitLimitOrder = (
+  options: ConstructProviderFetchInput<any, 'signTypedDataCall'>
+): SubmitLimitOrderFuncs => {
+  const { buildLimitOrder } = constructBuildLimitOrder(options);
+  const { signLimitOrder } = constructSignLimitOrder(options);
+  const { postLimitOrder } = constructPostLimitOrder(options);
+
+  const submitLimitOrder: SubmitLimitOrder = async (
+    buildLimitOrderParams,
+    signal
+  ) => {
+    const orderData = buildLimitOrder(buildLimitOrderParams);
+    const signature = await signLimitOrder(orderData);
+
+    const orderWithSignature: LimitOrderToSend = {
+      ...orderData.data,
+      chainId: orderData.domain.chainId,
+      signature,
+    };
+
+    const newOrder = await postLimitOrder(orderWithSignature, signal);
+
+    console.log('newOrder created', newOrder);
+    return newOrder;
+  };
+
+  return { submitLimitOrder };
+};
