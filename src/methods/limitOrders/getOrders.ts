e4ae5d97673c231814afdd443f78ca71083f05b5
Velenir <velenir@gmail.com>
2022-04-26 15:59:30 +0300

    sync types with APi

--- src/methods/limitOrders/getOrders.ts
+++ src/methods/limitOrders/getOrders.ts
@@ -1,13 +1,17 @@
-// @TODO getOrder, getOrders from API or subgrah
+// @TODO getOrder, getOrders from API
 // onchain from contract can't distinguish between filled or cancelled
 import { API_URL } from '../../constants';
 import type { ConstructFetchInput } from '../../types';
-import type { LimitOrdersApiResponse, RawLimitOrder } from './helpers/types';
+import type {
+  LimitOrderFromApi,
+  LimitOrdersApiResponse,
+  RawLimitOrder,
+} from './helpers/types';
 
 type GetLimitOrders = (
   userAddress: string,
   signal?: AbortSignal
-) => Promise<LimitOrdersApiResponse>;
+) => Promise<LimitOrderFromApi[]>;
 type GetRawLimitOrders = (
   userAddress: string,
   signal?: AbortSignal
@@ -23,34 +27,35 @@ export const constructGetLimitOrders = ({
   network,
   fetcher,
 }: ConstructFetchInput): GetLimitOrdersFunctions => {
-  const baseFetchURL = `${apiURL}/limit-orders/${network}`;
+  const baseFetchURL = `${apiURL}/limit-orders/${network}`; // in mocked FE
+  // const baseFetchURL = `${apiURL}/orders`; // in API
 
   // @TODO add getLimitOrderStatus method
 
   const getLimitOrders: GetLimitOrders = async (userAddress, signal) => {
     const fetchURL = `${baseFetchURL}/${userAddress}`;
 
-    const data = await fetcher<LimitOrdersApiResponse>({
+    const { orders } = await fetcher<LimitOrdersApiResponse>({
       url: fetchURL,
       method: 'GET',
       signal,
     });
 
     // @TODO attach status to orders, with onchain calls
-    return data;
+    return orders;
   };
 
   const getRawLimitOrders: GetRawLimitOrders = async (userAddress, signal) => {
     const fetchURL = `${baseFetchURL}/${userAddress}`;
 
-    const data = await fetcher<LimitOrdersApiResponse>({
+    const { orders } = await fetcher<LimitOrdersApiResponse>({
       url: fetchURL,
       method: 'GET',
       signal,
     });
 
     // without ny extra calls, return  what API returns
-    return data;
+    return orders;
   };
 
   return { getLimitOrders, getRawLimitOrders };
--- src/methods/limitOrders/helpers/types.ts
+++ src/methods/limitOrders/helpers/types.ts
@@ -14,16 +14,36 @@ export type LimitOrder = {
   chainId: number;
 };
 
-export type RawLimitOrder = Omit<LimitOrder, 'status'>;
+export type RawLimitOrder = LimitOrderFromApi;
 
 export type LimitOrderToSend = Omit<LimitOrder, 'orderHash' | 'status'> & {
   signature: string;
 };
 
 // @TODO check what format Dates are and if we get them from bith POST /orders and GET /orders/:walletAddress
-export type LimitOrderFromApi = RawLimitOrder & {
-  createdAt: string; // Date
-  updatedAt: string; // Date
-};
+// export type LimitOrderFromApi = RawLimitOrder & {
+//   createdAt: string; // Date
+//   updatedAt: string; // Date
+// };
 
-export type LimitOrdersApiResponse = LimitOrder[];
+export type LimitOrdersApiResponse = { orders: LimitOrderFromApi[] };
+export type LimitOrderApiResponse = { order: LimitOrderFromApi[] };
+
+export type LimitOrderFromApi = {
+  chainId: number;
+  nonceAndMeta: string; // uiint
+  expiry: number; // timestamp
+  maker: string; // address
+  taker: string; // address
+  makerAsset: string; // address
+  takerAsset: string; // address
+  makerAmount: string; // wei
+  takerAmount: string; // wei
+  signature: string; // supplied by FE
+  permitTakerAsset: null | string; // address
+  permitMakerAsset: null | string; // address
+  orderHash: string; // hex string
+  // not returned for now
+  // updatedAt: number; // timestamp
+  createdAt: number; // timestamp
+};
