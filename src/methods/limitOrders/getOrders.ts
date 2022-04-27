84332aa7d9c61a948469c9a48d1af6ac35f2c922
Velenir <velenir@gmail.com>
2022-04-27 17:56:39 +0300

    add getLimitOrdersStatusAndAmountFilled to limitOrders/getOrders

--- src/methods/limitOrders/getOrders.ts
+++ src/methods/limitOrders/getOrders.ts
@@ -1,37 +1,136 @@
 // @TODO getOrder, getOrders from API
 // onchain from contract can't distinguish between filled or cancelled
+import type { BigNumber as EthersBigNumber } from 'ethers';
+import { assert } from 'ts-essentials';
 import { API_URL } from '../../constants';
 import { isFetcherError } from '../../helpers';
-import type { ConstructFetchInput } from '../../types';
+import { ExtractAbiMethodNames } from '../../helpers/misc';
 import type {
-  LimitOrderFromApi,
+  Address,
+  ConstructProviderFetchInput,
+  StaticCallOverrides,
+} from '../../types';
+import { chainId2verifyingContract } from './helpers/misc';
+import type {
+  LimitOrder,
   LimitOrdersApiResponse,
+  LimitOrderStatus,
   RawLimitOrder,
 } from './helpers/types';
 
 type GetLimitOrders = (
   userAddress: string,
   signal?: AbortSignal
-) => Promise<LimitOrderFromApi[]>;
+) => Promise<LimitOrder[]>;
 type GetRawLimitOrders = (
   userAddress: string,
   signal?: AbortSignal
 ) => Promise<RawLimitOrder[]>;
 
+type GetOrderStatusAndAmountFilled = (
+  orderMaker: Address,
+  order: Pick<RawLimitOrder, 'expiry' | 'makerAmount' | 'orderHash'>,
+  overrides?: StaticCallOverrides
+) => Promise<LimitOrderExtra>;
+type GetOrdersStatusAndAmountFilled = (
+  orderMaker: Address,
+  orders: Pick<RawLimitOrder, 'expiry' | 'makerAmount' | 'orderHash'>[],
+  overrides?: StaticCallOverrides
+) => Promise<LimitOrderExtra[]>;
+
 export type GetLimitOrdersFunctions = {
   getLimitOrders: GetLimitOrders;
   getRawLimitOrders: GetRawLimitOrders;
+  getLimitOrderStatusAndAmountFilled: GetOrderStatusAndAmountFilled;
+  getLimitOrdersStatusAndAmountFilled: GetOrdersStatusAndAmountFilled;
 };
+
+const MinAugustusRFQAbi = [
+  {
+    inputs: [
+      {
+        internalType: 'address',
+        name: 'maker',
+        type: 'address',
+      },
+      {
+        internalType: 'bytes32[]',
+        name: 'orderHashes',
+        type: 'bytes32[]',
+      },
+    ],
+    name: 'getRemainingOrderBalance',
+    outputs: [
+      {
+        internalType: 'uint256[]',
+        name: 'remainingBalances',
+        type: 'uint256[]',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+] as const;
+
+type AvailableMethods = ExtractAbiMethodNames<typeof MinAugustusRFQAbi>;
 
 export const constructGetLimitOrders = ({
   apiURL = API_URL,
   network,
   fetcher,
-}: ConstructFetchInput): GetLimitOrdersFunctions => {
+  contractCaller,
+}: ConstructProviderFetchInput<any, 'staticCall'>): GetLimitOrdersFunctions => {
   const baseFetchURL = `${apiURL}/limit-orders/${network}`; // in mocked FE
   // const baseFetchURL = `${apiURL}/orders`; // in API
 
-  // @TODO add getLimitOrderStatus method
+  const verifyingContract = chainId2verifyingContract[network];
+
+  const getLimitOrdersStatusAndAmountFilled: GetOrdersStatusAndAmountFilled =
+    async (orderMaker, orders, overrides = {}) => {
+      assert(
+        verifyingContract,
+        `AugustusRFQ contract for Limit Orders not available on chain ${network}`
+      );
+
+      const orderHashes = orders.map((order) => order.orderHash);
+
+      // @TODO check what the return is for web3, probably string[]
+      const remainingBalances = await contractCaller.staticCall<
+        EthersBigNumber[],
+        AvailableMethods
+      >({
+        address: verifyingContract,
+        abi: MinAugustusRFQAbi,
+        contractMethod: 'getRemainingOrderBalance',
+        args: [orderMaker, orderHashes],
+        overrides,
+      });
+
+      const orderStatusesAndAmountsFilled = orders.map((order, index) => {
+        const remainingBalance = remainingBalances[index];
+        assert(
+          remainingBalance,
+          `Failed to get remainingBalance for order ${order.orderHash}`
+        );
+
+        return _getLimitOrderStatusAndAmountFilled(remainingBalance, order);
+      });
+
+      return orderStatusesAndAmountsFilled;
+    };
+
+  const getLimitOrderStatusAndAmountFilled: GetOrderStatusAndAmountFilled =
+    async (orderMaker, order, overrides = {}) => {
+      const [orderStatus] = await getLimitOrdersStatusAndAmountFilled(
+        orderMaker,
+        [order],
+        overrides
+      );
+
+      assert(orderStatus, `Failed to get status for order ${order.orderHash}`);
+
+      return orderStatus;
+    };
 
   const getRawLimitOrders: GetRawLimitOrders = async (userAddress, signal) => {
     const fetchURL = `${baseFetchURL}/${userAddress}`;
@@ -64,9 +163,65 @@ export const constructGetLimitOrders = ({
   const getLimitOrders: GetLimitOrders = async (userAddress, signal) => {
     const orders = await getRawLimitOrders(userAddress, signal);
 
-    // @TODO attach status to orders, with onchain calls
-    return orders;
+    const orderExtras = await getLimitOrdersStatusAndAmountFilled(
+      userAddress,
+      orders
+    );
+
+    const ordersWithEtras = orders.map<LimitOrder>((order, index) => {
+      const extras = orderExtras[index];
+      assert(extras, `Failed to get status for order ${order.orderHash}`);
+
+      return {
+        ...order,
+        ...extras,
+      };
+    });
+
+    return ordersWithEtras;
   };
 
-  return { getLimitOrders, getRawLimitOrders };
+  return {
+    getLimitOrders,
+    getRawLimitOrders,
+    getLimitOrderStatusAndAmountFilled,
+    getLimitOrdersStatusAndAmountFilled,
+  };
 };
+
+type LimitOrderExtra = Pick<LimitOrder, 'status' | 'amountFilled'>;
+
+// RemainingBalance keeps track of remaining amounts of each Order
+// 0 -> order unfilled / not exists
+// 1 -> order filled / cancelled
+function _getLimitOrderStatusAndAmountFilled(
+  remainingBalance: EthersBigNumber,
+  { expiry, makerAmount }: Pick<RawLimitOrder, 'expiry' | 'makerAmount'>
+): LimitOrderExtra {
+  // order exists since we got it from API
+  if (remainingBalance.eq(0)) {
+    const status: LimitOrderStatus =
+      Date.now() / 1000 > expiry ? 'expired' : 'open';
+    // `expired` status means the Order expired without being filled (even partially)
+
+    return {
+      status,
+      amountFilled: '0',
+    };
+  }
+
+  // filled or cancelled
+  if (remainingBalance.eq(1)) {
+    // @TODO go through evens to distinguish filled or cancelled
+    return {
+      status: 'filled',
+      amountFilled: makerAmount,
+    };
+  }
+
+  // partially filled
+  return {
+    status: 'filled',
+    amountFilled: remainingBalance.sub(makerAmount).mul(-1).toString(),
+  };
+}
