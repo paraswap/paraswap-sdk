934aa3c00b66ea75aae46a1c33ce72d01a42a480
Velenir <velenir@gmail.com>
2022-04-29 17:40:05 +0300

    getOrders gets status too

--- src/methods/limitOrders/getOrders.ts
+++ src/methods/limitOrders/getOrders.ts
@@ -1,6 +1,6 @@
 // @TODO getOrder, getOrders from API
 // onchain from contract can't distinguish between filled or cancelled
-import type { BigNumber as EthersBigNumber } from 'ethers';
+import { BigNumber as EthersBigNumber } from 'ethers';
 import { assert } from 'ts-essentials';
 import { API_URL } from '../../constants';
 import { isFetcherError } from '../../helpers';
@@ -72,14 +72,94 @@ const MinAugustusRFQAbi = [
   },
 ] as const;
 
+// Without OrderFilledNFT event for now
+const AugustusRFQEventsAbi = [
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: 'address',
+        name: 'maker',
+        type: 'address',
+      },
+      {
+        indexed: false,
+        internalType: 'bytes32',
+        name: 'orderHash',
+        type: 'bytes32',
+      },
+    ],
+    name: 'OrderCancelled',
+    type: 'event',
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: 'bytes32',
+        name: 'orderHash',
+        type: 'bytes32',
+      },
+      {
+        indexed: true,
+        internalType: 'address',
+        name: 'maker',
+        type: 'address',
+      },
+      {
+        indexed: false,
+        internalType: 'address',
+        name: 'makerAsset',
+        type: 'address',
+      },
+      {
+        indexed: false,
+        internalType: 'uint256',
+        name: 'makerAmount',
+        type: 'uint256',
+      },
+      {
+        indexed: true,
+        internalType: 'address',
+        name: 'taker',
+        type: 'address',
+      },
+      {
+        indexed: false,
+        internalType: 'address',
+        name: 'takerAsset',
+        type: 'address',
+      },
+      {
+        indexed: false,
+        internalType: 'uint256',
+        name: 'takerAmount',
+        type: 'uint256',
+      },
+    ],
+    name: 'OrderFilled',
+    type: 'event',
+  },
+] as const;
+
 type AvailableMethods = ExtractAbiMethodNames<typeof MinAugustusRFQAbi>;
+
+const OrderFilledSig =
+  '0x6621486d9c28838df4a87d2cca5007bc2aaf6a5b5de083b1db8faf709302c473';
+const OrderCancelledSig =
+  '0x35974c4230d53fb4c6e8553fd900c88ba92747dbc689a79bcd6ba755cb936985';
 
 export const constructGetLimitOrders = ({
   apiURL = API_URL,
   network,
   fetcher,
   contractCaller,
-}: ConstructProviderFetchInput<any, 'staticCall'>): GetLimitOrdersFunctions => {
+}: ConstructProviderFetchInput<
+  any,
+  'staticCall' | 'getLogsCall'
+>): GetLimitOrdersFunctions => {
   // const baseFetchURL = `${apiURL}/limit-orders/${network}`; // in mocked FE
   const baseFetchURL = `${apiURL}/orders`; // in API
 
@@ -87,6 +167,7 @@ export const constructGetLimitOrders = ({
 
   const getLimitOrdersStatusAndAmountFilled: GetOrdersStatusAndAmountFilled =
     async (orderMaker, orders, overrides = {}) => {
+      // @TODO allow to pass {ordderHash} only, fetch everything else
       assert(
         verifyingContract,
         `AugustusRFQ contract for Limit Orders not available on chain ${network}`
@@ -106,6 +187,56 @@ export const constructGetLimitOrders = ({
         overrides,
       });
 
+      type OrderCancelledDecodedArgs = readonly [
+        maker: Address,
+        orderHash: string
+      ] & {
+        maker: Address;
+        orderHash: string;
+      };
+      interface OrderCancelleDecodedLog {
+        topic: string;
+        args: OrderCancelledDecodedArgs;
+      }
+      type OrderFilledDecodedArgs = readonly [
+        orderHash: string,
+        maker: Address,
+        makerAsset: Address,
+        makerAmount: EthersBigNumber, // @TODO see what works with web3, may need to just stick to string everywhere
+        taker: Address,
+        takerAsset: Address,
+        takerAmount: EthersBigNumber
+      ] & {
+        orderHash: string;
+        maker: Address;
+        makerAsset: Address;
+        makerAmount: EthersBigNumber;
+        taker: Address;
+        takerAsset: Address;
+        takerAmount: EthersBigNumber;
+      };
+      interface OrderFilledDecodedLog {
+        topic: string;
+        args: OrderFilledDecodedArgs;
+      }
+
+      const logs = (await contractCaller.getLogsCall({
+        address: verifyingContract,
+        abi: AugustusRFQEventsAbi,
+        filter: {
+          address: verifyingContract,
+          topics: [[OrderFilledSig, OrderCancelledSig]],
+          fromBlock: '0xba857e',
+          // fromBlock: 12223870,
+        },
+      })) as (OrderCancelleDecodedLog | OrderFilledDecodedLog)[]; // @TODO adapt for web3 too
+
+      const cancelledOrders = new Set(
+        logs
+          .filter((log) => log.topic === OrderCancelledSig)
+          .map((log) => log.args.orderHash)
+      );
+
       const orderStatusesAndAmountsFilled = orders.map((order, index) => {
         const remainingBalance = remainingBalances[index];
         assert(
@@ -113,7 +244,38 @@ export const constructGetLimitOrders = ({
           `Failed to get remainingBalance for order ${order.orderHash}`
         );
 
-        return _getLimitOrderStatusAndAmountFilled(remainingBalance, order);
+        const wasCancelled = cancelledOrders.has(order.orderHash);
+
+        const filledEvents = !wasCancelled
+          ? []
+          : logs.filter<OrderFilledDecodedLog>(
+              (log): log is OrderFilledDecodedLog =>
+                log.args.orderHash === order.orderHash &&
+                log.topic === OrderFilledSig
+            );
+
+        // @TODO check this is even correct
+        const _amountFilled =
+          filledEvents.length === 0
+            ? undefined
+            : // sum up all takerAmount across OrderFilled event for this orderHash
+              filledEvents.reduce(
+                (accum, curr) => accum.add(curr.args.takerAmount),
+                EthersBigNumber.from(0) // switch to string so we don't have a hard ethers dependency
+              );
+
+        const amountFilled = !_amountFilled
+          ? undefined
+          : _amountFilled.isZero()
+          ? undefined
+          : _amountFilled.toString();
+
+        return _getLimitOrderStatusAndAmountFilled(
+          remainingBalance,
+          order,
+          wasCancelled,
+          amountFilled
+        );
       });
 
       return orderStatusesAndAmountsFilled;
@@ -196,8 +358,11 @@ type LimitOrderExtra = Pick<LimitOrder, 'status' | 'amountFilled'>;
 // 1 -> order filled / cancelled
 function _getLimitOrderStatusAndAmountFilled(
   remainingBalance: EthersBigNumber,
-  { expiry, makerAmount }: Pick<RawLimitOrder, 'expiry' | 'makerAmount'>
+  { expiry, makerAmount }: Pick<RawLimitOrder, 'expiry' | 'makerAmount'>,
+  wasCancelled: boolean,
+  amountFilled?: string
 ): LimitOrderExtra {
+  console.log('🚀 ~ remainingBalance', remainingBalance.toString());
   // order exists since we got it from API
   if (remainingBalance.eq(0)) {
     const status: LimitOrderStatus =
@@ -212,10 +377,11 @@ function _getLimitOrderStatusAndAmountFilled(
 
   // filled or cancelled
   if (remainingBalance.eq(1)) {
-    // @TODO go through evens to distinguish filled or cancelled
+    // @TODO go through events to distinguish filled or cancelled
+    // @TODO check if cancelled was partially filled first
     return {
-      status: 'filled',
-      amountFilled: makerAmount,
+      status: wasCancelled ? 'canceled' : 'filled',
+      amountFilled: wasCancelled && amountFilled ? amountFilled : makerAmount,
     };
   }
 
