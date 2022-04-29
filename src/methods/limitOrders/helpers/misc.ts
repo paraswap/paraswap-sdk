7573eee1b48fd420ccd31c96f4dec0ebc58dbe11
Velenir <velenir@gmail.com>
2022-04-29 17:18:46 +0300

    add ropsten contract

--- src/methods/limitOrders/helpers/misc.ts
+++ src/methods/limitOrders/helpers/misc.ts
@@ -3,4 +3,5 @@ import type { Address } from '../../../types';
 // @TODO either fill in or fetch from API
 export const chainId2verifyingContract: Record<number, Address> = {
   1: '0xDEF171Fe48CF0115B1d80b88dc8eAB59176FEe57',
+  3: '0xe10ada2399359f004264c9d399a1DD6E83eD6660',
 };
