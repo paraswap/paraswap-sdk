7d86de3e12def953dcd20354cc00bff69ce707be
Velenir <velenir@gmail.com>
2022-05-05 19:11:44 +0300

    update Ropsten contract

--- src/methods/limitOrders/helpers/misc.ts
+++ src/methods/limitOrders/helpers/misc.ts
@@ -3,9 +3,9 @@ import type { Address } from '../../../types';
 // @TODO either fill in or fetch from API
 export const chainId2verifyingContract: Record<number, Address> = {
   1: '0xDEF171Fe48CF0115B1d80b88dc8eAB59176FEe57',
-  3: '0xe10ada2399359f004264c9d399a1DD6E83eD6660',
+  3: '0x1944d770dE8Bcf958305C68FA821936e16af35a6',
 };
 
 export const chainId2BlockContractDeployedAt: Record<number, number> = {
-  3: 12223870,
+  3: 12240016,
 };
