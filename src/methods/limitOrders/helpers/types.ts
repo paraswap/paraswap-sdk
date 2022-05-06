3d84270c21edffb62e375aa3727abe072612f232
Velenir <velenir@gmail.com>
2022-05-06 17:26:08 +0300

    a bunch of helper types to discriminate between different Orders

--- src/methods/limitOrders/helpers/types.ts
+++ src/methods/limitOrders/helpers/types.ts
@@ -1,3 +1,5 @@
+import type { NonNever } from 'ts-essentials';
+
 export type LimitOrderStatus =
   | 'open'
   | 'filled'
@@ -5,28 +7,79 @@ export type LimitOrderStatus =
   | 'expired'
   | 'canceled'
   | 'unknown';
+
 export type LimitOrder = LimitOrderFromApi & {
   status: LimitOrderStatus; // `status` likely not available from API
-  amountFilled: string;
+  amountFilled?: string;
   transactionHashes?: string[];
 };
 
-export type ExpiredLimitOrder = LimitOrderFromApi & { status: 'expired' };
-export type OpenLimitOrder = LimitOrderFromApi & { status: 'open' };
+export type ExpiredLimitOrder = LimitOrderFromApi & {
+  status: 'expired';
+  amountFilled: '0';
+};
+export type OpenLimitOrder = LimitOrderFromApi & {
+  status: 'open';
+  amountFilled: '0';
+};
 export type UnknownLimitOrder = LimitOrderFromApi & { status: 'unknown' };
 export type FilledLimitOrder = LimitOrderFromApi & {
   status: 'filled';
+  amountFilled: string;
   transactionHashes: [string, ...string[]];
 };
 export type PartiallyFilledLimitOrder = LimitOrderFromApi & {
   status: 'partiallyFilled';
+  amountFilled: string;
   transactionHashes: [string, ...string[]];
 };
 export type CancelledLimitOrder = LimitOrderFromApi & {
   status: 'canceled';
+  amountFilled: string;
   /** @description  last tx hash is always for OrderCancelled event */
   transactionHashes: [string, ...string[]]; // last tx hash is always for OrderCancelled event
 };
+
+// these Orders can be discriminated by `status`, unlike the generic LimitOrder
+//  any of them is assignable to LimitOrder
+export type AnyLimitOrder =
+  | ExpiredLimitOrder
+  | OpenLimitOrder
+  | FilledLimitOrder
+  | PartiallyFilledLimitOrder
+  | CancelledLimitOrder;
+
+type PickExistingKeys<T, K extends string> = NonNever<{
+  [P in K]: P extends keyof T ? T[P] : never;
+}>;
+
+type _PickExistingKeysInArrayHelper<
+  T extends Record<string, any>[],
+  K extends string,
+  Accum
+> = T extends [head: infer Head, ...tail: infer Tail]
+  ? Tail extends Record<string, any>[]
+    ? _PickExistingKeysInArrayHelper<Tail, K, PickExistingKeys<Head, K> | Accum>
+    : PickExistingKeys<Head, K> | Accum
+  : Accum;
+
+// returns union of array elements with existing keys priocked from K given
+type PickExistingKeysInArray<
+  T extends Record<string, any>[],
+  K extends string
+> = _PickExistingKeysInArrayHelper<T, K, never>;
+
+// union of {status, amountFilled?, transactionHashes?} correctly corresponding to each Order type
+export type LimitOrderExtra = PickExistingKeysInArray<
+  [
+    ExpiredLimitOrder,
+    OpenLimitOrder,
+    FilledLimitOrder,
+    PartiallyFilledLimitOrder,
+    CancelledLimitOrder
+  ],
+  'status' | 'amountFilled' | 'transactionHashes'
+>;
 
 export type RawLimitOrder = LimitOrderFromApi;
 
@@ -38,15 +91,11 @@ export type LimitOrderToSend = Omit<
   | 'createdAt'
   | 'permitMakerAsset'
   | 'permitTakerAsset'
+  | 'transactionHashes'
 > & {
   signature: string;
+  permitMakerAsset?: string;
 };
-
-// @TODO check what format Dates are and if we get them from bith POST /orders and GET /orders/:walletAddress
-// export type LimitOrderFromApi = RawLimitOrder & {
-//   createdAt: string; // Date
-//   updatedAt: string; // Date
-// };
 
 export type LimitOrdersApiResponse = {
   orders: LimitOrderFromApi[];
