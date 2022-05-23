dd570b24f8e862d9936f8c12d8145396de058bbf
Velenir <velenir@gmail.com>
2022-05-23 17:01:30 +0200

    update contract address

--- src/methods/limitOrders/helpers/misc.ts
+++ src/methods/limitOrders/helpers/misc.ts
@@ -4,7 +4,7 @@ import { OrderData } from './buildOrderData';
 // @TODO either fill in or fetch from API
 export const chainId2verifyingContract: Record<number, Address> = {
   1: '0xDEF171Fe48CF0115B1d80b88dc8eAB59176FEe57',
-  3: '0x1944d770dE8Bcf958305C68FA821936e16af35a6',
+  3: '0xA21eBa22ab9EBE4eCE0DaEBAaBEb5F8098a404c9',
 };
 
 export const chainId2BlockContractDeployedAt: Record<number, number> = {
