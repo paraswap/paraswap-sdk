8d80f86f7a8c9df33f8383a7c252bfce375b524c
Velenir <velenir@gmail.com>
2022-05-27 15:56:27 +0200

    update FullSDK = {swap, limitOrders}

--- src/sdk/full.ts
+++ src/sdk/full.ts
@@ -1,18 +1,16 @@
+import type { ApproveTokenFunctions } from '../methods/swap/approve';
+import type { GetBalancesFunctions } from '../methods/swap/balance';
+import type { GetSpenderFunctions } from '../methods/swap/spender';
+import type { GetAdaptersFunctions } from '../methods/swap/adapters';
+import type { GetRateFunctions } from '../methods/swap/rates';
+import type { GetTokensFunctions } from '../methods/swap/token';
+import type { BuildTxFunctions } from '../methods/swap/transaction';
+import type { SDKConfig } from './partial';
+import { constructSwapSDK } from '../methods/swap';
 import {
-  constructApproveToken,
-  ApproveTokenFunctions,
-} from '../methods/approve';
-import { constructGetBalances, GetBalancesFunctions } from '../methods/balance';
-import { constructGetSpender, GetSpenderFunctions } from '../methods/spender';
-import {
-  constructGetAdapters,
-  GetAdaptersFunctions,
-} from '../methods/adapters';
-import { constructGetRate, GetRateFunctions } from '../methods/rates';
-import { constructGetTokens, GetTokensFunctions } from '../methods/token';
-import { constructBuildTx, BuildTxFunctions } from '../methods/transaction';
-import { constructPartialSDK, SDKConfig } from './partial';
-import type { ConstructProviderFetchInput } from '../types';
+  constructAllLimitOrdersHandlers,
+  LimitOrderHandlers,
+} from '../methods/limitOrders';
 
 export type SwapSDKMethods<TxResponse> = GetBalancesFunctions &
   GetTokensFunctions &
@@ -22,23 +20,19 @@ export type SwapSDKMethods<TxResponse> = GetBalancesFunctions &
   GetAdaptersFunctions &
   GetRateFunctions;
 
-export type AllSDKMethods<TxResponse> = SwapSDKMethods<TxResponse>;
+export type AllSDKMethods<TxResponse> = {
+  swap: SwapSDKMethods<TxResponse>;
+  limitOrders: LimitOrderHandlers<TxResponse>;
+};
 
-/** @description construct SDK with every method, fetching from API and token approval */
+/** @description construct SDK with every method, for swap and limitOrders */
 export const constructFullSDK = <TxResponse = any>(
   config: SDKConfig<TxResponse>
-): AllSDKMethods<TxResponse> =>
+): AllSDKMethods<TxResponse> => {
   // include all available functions
-  constructPartialSDK(
-    config,
-    constructGetBalances,
-    constructGetTokens,
-    constructGetSpender,
-    constructApproveToken as (
-      options: ConstructProviderFetchInput<TxResponse, 'transactCall'>
-    ) => ApproveTokenFunctions<TxResponse>, // @TODO try Instantiation Expression when TS 4.7 `as constructApproveToken<TxResponse>`
-    constructBuildTx,
-    constructGetAdapters,
-    constructGetRate
-    // @TODO fill in limitOrders methods
-  );
+  const swap: SwapSDKMethods<TxResponse> = constructSwapSDK(config);
+  const limitOrders: LimitOrderHandlers<TxResponse> =
+    constructAllLimitOrdersHandlers(config);
+
+  return { swap, limitOrders };
+};
