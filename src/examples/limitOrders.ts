994ceb2a108003312588152f6f14066699d76425
Velenir <velenir@gmail.com>
2022-05-27 09:34:37 +0200

    examples/limitOrders usability changes

--- src/examples/limitOrders.ts
+++ src/examples/limitOrders.ts
@@ -6,7 +6,6 @@ import {
   constructPartialSDK,
   constructEthersContractCaller,
   constructAxiosFetcher,
-  constructApproveToken,
   // limitOrders methods
   constructBuildLimitOrder,
   constructCancelLimitOrder,
@@ -19,26 +18,32 @@ import {
   SignableOrderData,
 } from '..';
 
+const account = '0x1234...';
+
 const fetcher = constructAxiosFetcher(axios);
 
+// provider must have write access to account
+// this would usually be wallet provider (Metamask)
 const provider = ethers.getDefaultProvider(1);
-const contractCaller = constructEthersContractCaller({
-  ethersProviderOrSigner: provider,
-  EthersContract: ethers.Contract,
-});
+const contractCaller = constructEthersContractCaller(
+  {
+    ethersProviderOrSigner: provider,
+    EthersContract: ethers.Contract,
+  },
+  account
+);
 
 // type BuildLimitOrderFunctions
 // & SignLimitOrderFunctions
 // & CancelLimitOrderFunctions<ethers.ContractTransaction>
 // & ApproveTokenFunctions<ethers.ContractTransaction>
-const part1 = constructPartialSDK(
+const paraSwapLimitOrderSDK = constructPartialSDK(
   {
     network: 1,
     fetcher,
     contractCaller,
   },
   constructBuildLimitOrder,
-  constructApproveToken,
   constructCancelLimitOrder,
   constructSignLimitOrder,
   constructPostLimitOrder,
@@ -47,42 +52,67 @@ const part1 = constructPartialSDK(
   constructApproveTokenForLimitOrder
 );
 
+const DAI = '0x6B175474E89094C44Da98b954EedeAC495271d0F';
+const HEX = '0x2b591e99afe9f32eaa6214f7b7629768c40eeb39';
+
 const orderInput = {
   nonce: 1,
   expiry: Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 7, // week from now, in sec
-  makerAsset: '0x1234...',
-  takerAsset: '0x1234...',
+  makerAsset: DAI,
+  takerAsset: HEX,
   makerAmount: (1e18).toString(10),
   takerAmount: (8e18).toString(10),
-  maker: '0x1234...',
+  maker: account,
 };
 
 async function run() {
-  const signableOrderData: SignableOrderData =
-    part1.buildLimitOrder(orderInput);
-  // type string
-  const signature: string = await part1.signLimitOrder(signableOrderData);
-
-  const tx1: ethers.ContractTransaction = await part1.cancelLimitOrder(
-    'order hash bytes32'
-  );
-  const tx2: ethers.ContractTransaction = await part1.cancelLimitOrderBulk([
-    'order hash bytes32',
-    'more order hashes',
-  ]);
-
-  const tx3: ethers.ContractTransaction = await part1.approveToken(
-    '1234',
-    '0x1234'
-  );
-
-  const tx4: ethers.ContractTransaction = await part1.approveTokenForLimitOrder(
-    '1234',
-    '0x1234'
-  );
-
-  const tx5: ethers.ContractTransaction = await part1.fillLimitOrder({
-    orderData: signableOrderData.data,
-    signature,
+  const currentOrders = await paraSwapLimitOrderSDK.getLimitOrders({
+    maker: account,
   });
+
+  const signableOrderData: SignableOrderData =
+    paraSwapLimitOrderSDK.buildLimitOrder(orderInput);
+  // type string
+  const signature: string = await paraSwapLimitOrderSDK.signLimitOrder(
+    signableOrderData
+  );
+
+  if (currentOrders[0]?.orderHash) {
+    const tx1: ethers.ContractTransaction =
+      await paraSwapLimitOrderSDK.cancelLimitOrder(currentOrders[0].orderHash);
+  }
+
+  const moreOrderHashes = currentOrders
+    .slice(1, 2)
+    .map((order) => order.orderHash);
+
+  const tx2: ethers.ContractTransaction =
+    await paraSwapLimitOrderSDK.cancelLimitOrderBulk(moreOrderHashes);
+
+  const tx3: ethers.ContractTransaction =
+    await paraSwapLimitOrderSDK.approveTokenForLimitOrder('1234', '0x1234');
+
+  // to act as order taker
+  const anotherAccount = '0x5678...';
+
+  const paraswapLimitOrdersSDKForTaker = constructPartialSDK(
+    {
+      network: 1,
+      fetcher,
+      contractCaller: constructEthersContractCaller(
+        {
+          ethersProviderOrSigner: provider,
+          EthersContract: ethers.Contract,
+        },
+        anotherAccount
+      ),
+    },
+    constructFillLimitOrder
+  );
+
+  const tx4: ethers.ContractTransaction =
+    await paraswapLimitOrdersSDKForTaker.fillLimitOrder({
+      orderData: signableOrderData.data,
+      signature,
+    });
 }
