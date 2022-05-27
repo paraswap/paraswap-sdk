42288ed5b4656295ac8aa48ff46bdc948997e5a5
Velenir <velenir@gmail.com>
2022-05-27 14:17:21 +0200

    add contract addresses

--- src/methods/limitOrders/helpers/misc.ts
+++ src/methods/limitOrders/helpers/misc.ts
@@ -3,8 +3,12 @@ import { OrderData } from './buildOrderData';
 
 // @TODO either fill in or fetch from API
 export const chainId2verifyingContract: Record<number, Address> = {
-  1: '0xDEF171Fe48CF0115B1d80b88dc8eAB59176FEe57',
-  3: '0xA21eBa22ab9EBE4eCE0DaEBAaBEb5F8098a404c9',
+  1: '0xe92b586627ccA7a83dC919cc7127196d70f55a06', // Mainnet
+  3: '0xA21eBa22ab9EBE4eCE0DaEBAaBEb5F8098a404c9', // Ropsten
+  56: '0x8DcDfe88EF0351f27437284D0710cD65b20288bb', // BSC
+  137: '0xF3CD476C3C4D3Ac5cA2724767f269070CA09A043', // Polygon
+  250: '0x2DF17455B96Dde3618FD6B1C3a9AA06D6aB89347', // Fantom
+  43114: '0x34302c4267d0dA0A8c65510282Cc22E9e39df51f', // Avalanche
 };
 
 export const chainId2BlockContractDeployedAt: Record<number, number> = {
