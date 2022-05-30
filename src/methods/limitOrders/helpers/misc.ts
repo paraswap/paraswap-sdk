895739fa252a6ce53c9e4f384ec4e11e36f22a53
Velenir <velenir@gmail.com>
2022-05-30 13:20:52 +0200

    update contract on ropsten

--- src/methods/limitOrders/helpers/misc.ts
+++ src/methods/limitOrders/helpers/misc.ts
@@ -4,7 +4,7 @@ import { OrderData } from './buildOrderData';
 // @TODO either fill in or fetch from API
 export const chainId2verifyingContract: Record<number, Address> = {
   1: '0xe92b586627ccA7a83dC919cc7127196d70f55a06', // Mainnet
-  3: '0xA21eBa22ab9EBE4eCE0DaEBAaBEb5F8098a404c9', // Ropsten
+  3: '0x34268C38fcbC798814b058656bC0156C7511c0E4', // Ropsten
   56: '0x8DcDfe88EF0351f27437284D0710cD65b20288bb', // BSC
   137: '0xF3CD476C3C4D3Ac5cA2724767f269070CA09A043', // Polygon
   250: '0x2DF17455B96Dde3618FD6B1C3a9AA06D6aB89347', // Fantom
