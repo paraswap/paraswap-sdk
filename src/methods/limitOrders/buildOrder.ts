9af346d4e6cfa4b6f851bd8ecb2394ef6135c2e1
Velenir <velenir@gmail.com>
2022-06-03 16:24:45 +0200

    fetch AugustusAddress inside buildLimitOrder

--- src/methods/limitOrders/buildOrder.ts
+++ src/methods/limitOrders/buildOrder.ts
@@ -1,6 +1,7 @@
 import { assert } from 'ts-essentials';
 // import { API_URL } from '../../constants';
-import { ConstructBaseInput } from '../../types';
+import type { ConstructFetchInput } from '../../types';
+import { constructGetSpender } from '../swap/spender';
 import {
   buildOrderData,
   BuildOrderDataInput,
@@ -11,32 +12,48 @@ export * from './helpers/buildOrderData';
 
 export type BuildLimitOrderInput = Omit<
   BuildOrderDataInput,
-  'chainId' | 'verifyingContract'
+  'chainId' | 'verifyingContract' | 'AugustusAddress'
 >;
 
 type BuildLimitOrder = (
-  buildLimitOrderParams: BuildLimitOrderInput
-) => SignableOrderData;
+  buildLimitOrderParams: BuildLimitOrderInput,
+  signal?: AbortSignal
+) => Promise<SignableOrderData>;
 
 export type BuildLimitOrderFunctions = {
   buildLimitOrder: BuildLimitOrder;
 };
 
-export const constructBuildLimitOrder = ({
-  // apiURL = API_URL,
-  chainId,
-}: Pick<ConstructBaseInput, 'chainId'>): BuildLimitOrderFunctions => {
+export const constructBuildLimitOrder = (
+  options: ConstructFetchInput
+): BuildLimitOrderFunctions => {
+  const { chainId } = options;
   const verifyingContract = chainId2verifyingContract[chainId];
 
-  const buildLimitOrder: BuildLimitOrder = (buildLimitOrderParams) => {
+  const { getSpender: _getAugustusAddress } = constructGetSpender(options);
+  // cached for the same instance of `buildLimitOrder = constructBuildLimitOrder()`
+  // so should persist across same apiUrl & network
+  let _spender: string | undefined;
+
+  const getAugustusAddress: typeof _getAugustusAddress = async (signal) =>
+    _spender || (_spender = await _getAugustusAddress(signal));
+
+  const buildLimitOrder: BuildLimitOrder = async (
+    buildLimitOrderParams,
+    signal
+  ) => {
     assert(
       verifyingContract,
       `verifyingContract for Limit Orders not available on chain ${chainId}`
     );
+
+    const AugustusAddress = await getAugustusAddress(signal);
+
     return buildOrderData({
       ...buildLimitOrderParams,
       chainId,
       verifyingContract,
+      AugustusAddress,
     });
   };
 
