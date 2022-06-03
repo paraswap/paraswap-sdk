6c4110dc629ef90c33300a9aa15bb8dd46a25f14
Velenir <velenir@gmail.com>
2022-06-03 16:58:17 +0200

    add getContracts & getAugustusSwapper to constructGetSpender

--- src/methods/swap/spender.ts
+++ src/methods/swap/spender.ts
@@ -2,9 +2,12 @@ import { API_URL } from '../../constants';
 import type { ConstructFetchInput, Address } from '../../types';
 
 type GetSpender = (signal?: AbortSignal) => Promise<Address>;
+type GetContracts = (signal?: AbortSignal) => Promise<AdaptersContractsResult>;
 
 export type GetSpenderFunctions = {
   getSpender: GetSpender;
+  getAugustusSwapper: GetSpender;
+  getContracts: GetContracts;
 };
 
 interface AdaptersContractsResult {
@@ -19,15 +22,25 @@ export const constructGetSpender = ({
 }: ConstructFetchInput): GetSpenderFunctions => {
   const fetchURL = `${apiURL}/adapters/contracts?network=${chainId}`;
 
-  const getSpender: GetSpender = async (signal) => {
+  const getContracts: GetContracts = async (signal) => {
     const data = await fetcher<AdaptersContractsResult>({
       url: fetchURL,
       method: 'GET',
       signal,
     });
 
-    return data.TokenTransferProxy;
+    return data;
   };
 
-  return { getSpender };
+  const getSpender: GetSpender = async (signal) => {
+    const { TokenTransferProxy } = await getContracts(signal);
+    return TokenTransferProxy;
+  };
+
+  const getAugustusSwapper: GetSpender = async (signal) => {
+    const { AugustusSwapper } = await getContracts(signal);
+    return AugustusSwapper;
+  };
+
+  return { getContracts, getSpender, getAugustusSwapper };
 };
