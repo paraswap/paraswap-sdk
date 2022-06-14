76ae7d66d3cfb7b971acd36bd06ae106cbd0df79
Velenir <velenir@gmail.com>
2022-06-14 14:19:31 +0200

    build order method

---
+++ src/methods/nftOrders/buildOrder.ts
@@ -0,0 +1,61 @@
+import { assert } from 'ts-essentials';
+import type { ConstructFetchInput } from '../../types';
+import { constructGetSpender } from '../swap/spender';
+import {
+  buildOrderData,
+  BuildOrderDataInput,
+  SignableOrderData,
+} from './helpers/buildOrderData';
+import { chainId2verifyingContract } from './helpers/misc';
+export * from './helpers/buildOrderData';
+
+export type BuildNFTOrderInput = Omit<
+  BuildOrderDataInput,
+  'chainId' | 'verifyingContract' | 'AugustusAddress'
+>;
+
+type BuildNFTOrder = (
+  buildNFTOrderParams: BuildNFTOrderInput,
+  signal?: AbortSignal
+) => Promise<SignableOrderData>;
+
+export type BuildNFTOrderFunctions = {
+  /** @description Build Orders that will be excuted through AugustusSwapper */
+  buildNFTOrder: BuildNFTOrder;
+};
+
+export const constructBuildNFTOrder = (
+  options: ConstructFetchInput
+): BuildNFTOrderFunctions => {
+  const { chainId } = options;
+  const verifyingContract = chainId2verifyingContract[chainId];
+
+  const { getAugustusSwapper: _getAugustusAddress } =
+    constructGetSpender(options);
+  // cached for the same instance of `buildNFTOrder = constructBuildNFTOrder()`
+  // so should persist across same apiUrl & network
+  let _spender: string | undefined;
+
+  const getAugustusAddress: typeof _getAugustusAddress = async (signal) =>
+    _spender || (_spender = await _getAugustusAddress(signal));
+
+  const buildNFTOrder: BuildNFTOrder = async (buildNFTOrderParams, signal) => {
+    assert(
+      verifyingContract,
+      `verifyingContract for NFT Orders not available on chain ${chainId}`
+    );
+
+    const AugustusAddress = await getAugustusAddress(signal);
+
+    return buildOrderData({
+      ...buildNFTOrderParams,
+      chainId,
+      verifyingContract,
+      AugustusAddress,
+    });
+  };
+
+  return {
+    buildNFTOrder,
+  };
+};
