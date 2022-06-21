9ebe8135f5dd3aa83761216ee2c98e59412a9c18
Velenir <velenir@gmail.com>
2022-06-21 18:13:56 +0200

    remove unused types

--- src/methods/limitOrders/helpers/types.ts
+++ src/methods/limitOrders/helpers/types.ts
@@ -1,4 +1,3 @@
-import type { NonNever } from 'ts-essentials';
 import type { OrderData } from './buildOrderData';
 
 export type LimitOrderStatus =
@@ -16,76 +15,6 @@ export type LimitOrder = LimitOrderFromApi & {
   amountFilled?: string;
   transactionHashes?: string[];
 };
-
-export type ExpiredLimitOrder = LimitOrderFromApi & {
-  status: 'expired';
-  amountFilled: '0';
-};
-export type OpenLimitOrder = LimitOrderFromApi & {
-  status: 'open';
-  amountFilled: '0';
-};
-export type UnknownLimitOrder = LimitOrderFromApi & { status: 'unknown' };
-export type FilledLimitOrder = LimitOrderFromApi & {
-  status: 'filled';
-  amountFilled: string;
-  transactionHashes: [string, ...string[]];
-};
-export type PartiallyFilledLimitOrder = LimitOrderFromApi & {
-  status: 'partiallyFilled';
-  amountFilled: string;
-  transactionHashes: [string, ...string[]];
-};
-export type CancelledLimitOrder = LimitOrderFromApi & {
-  status: 'canceled';
-  amountFilled: string;
-  /** @description  last tx hash is always for OrderCancelled event */
-  transactionHashes: [string, ...string[]]; // last tx hash is always for OrderCancelled event
-};
-
-// these Orders can be discriminated by `status`, unlike the generic LimitOrder
-//  any of them is assignable to LimitOrder
-export type AnyLimitOrder =
-  | ExpiredLimitOrder
-  | OpenLimitOrder
-  | FilledLimitOrder
-  | PartiallyFilledLimitOrder
-  | CancelledLimitOrder;
-
-type PickExistingKeys<T, K extends string> = NonNever<{
-  [P in K]: P extends keyof T ? T[P] : never;
-}>;
-
-type _PickExistingKeysInArrayHelper<
-  T extends Record<string, any>[],
-  K extends string,
-  Accum
-> = T extends [head: infer Head, ...tail: infer Tail]
-  ? Tail extends Record<string, any>[]
-    ? _PickExistingKeysInArrayHelper<Tail, K, PickExistingKeys<Head, K> | Accum>
-    : PickExistingKeys<Head, K> | Accum
-  : Accum;
-
-// returns union of array elements with existing keys picked from K given
-type PickExistingKeysInArray<
-  T extends Record<string, any>[],
-  K extends string
-> = _PickExistingKeysInArrayHelper<T, K, never>;
-
-// union of {status, amountFilled?, transactionHashes?} correctly corresponding to each Order type
-export type LimitOrderExtra = PickExistingKeysInArray<
-  [
-    ExpiredLimitOrder,
-    OpenLimitOrder,
-    FilledLimitOrder,
-    PartiallyFilledLimitOrder,
-    CancelledLimitOrder
-  ],
-  'status' | 'amountFilled' | 'transactionHashes'
->;
-
-export type RawLimitOrder = LimitOrderFromApi;
-
 export type LimitOrderToSend = OrderData & {
   permitMakerAsset?: string;
   signature: string;
