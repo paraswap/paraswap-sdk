9cb28fe55e284889d463c35c0db95c725f4e9ed5
alexshchur <60445720+alexshchur@users.noreply.github.com>
2022-09-23 08:42:21 +0300

    remove test of an obsolete endpoint

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -234,20 +234,6 @@ describe.each([
     expect(allowance.allowance).toEqual('123000000000000000');
   });
 
-  test('Get_Allowances', async () => {
-    const allowances = await paraSwap.getAllowances(
-      DUMMY_ADDRESS_FOR_TESTING_ALLOWANCES,
-      [DAI, HEX]
-    );
-
-    const [daiAllowance, hexAllowance] = allowances.map(
-      (allowance) => allowance.allowance
-    );
-
-    expect(daiAllowance).toEqual('123000000000000000');
-    expect(hexAllowance).toEqual('32100000');
-  });
-
   test('Get_Adapters', async () => {
     const adapters = await paraSwap.getAdapters({ type: 'object' });
     expect(adapters.paraswappool?.[0]?.adapter).toBeDefined();
