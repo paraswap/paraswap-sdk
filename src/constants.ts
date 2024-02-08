403f713ef3c0ef3c8b57f21daf312a8f54b426fb
Velenir <velenir@gmail.com>
2024-02-08 13:21:34 +0100

    Version types

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,6 @@
+import { ParaSwapVersionUnion } from './types';
+
 export { SwapSide, ContractMethod } from '@paraswap/core';
 
 export const API_URL = 'https://api.paraswap.io';
+export const DEFAULT_VERSION: ParaSwapVersionUnion = '5';
