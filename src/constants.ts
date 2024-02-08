915f0058afe00e2a2211ebaf144fb218d8c2e17c
Velenir <velenir@gmail.com>
2024-02-08 14:43:29 +0100

    reexport ContractMethodV5 & ContractMethodV6

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,11 @@
 import { ParaSwapVersionUnion } from './types';
 
-export { SwapSide, ContractMethod } from '@paraswap/core';
+export {
+  SwapSide,
+  ContractMethod,
+  ContractMethodV5,
+  ContractMethodV6,
+} from '@paraswap/core';
 
 export const API_URL = 'https://api.paraswap.io';
 export const DEFAULT_VERSION: ParaSwapVersionUnion = '5';
