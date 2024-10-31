641005ca21f9129284f891a569c3ddca5d7eff54
Velenir <velenir@gmail.com>
2024-10-31 13:54:46 +0100

    simplify hardhat helpers

--- tests/helpers/hardhat.ts
+++ tests/helpers/hardhat.ts
@@ -1,11 +1,7 @@
 import hre from 'hardhat';
 import type { EthereumProvider } from 'hardhat/types';
-
-interface ForkChainInput {
-  rpcUrl?: string;
-  blockNumber?: number;
-  chainId?: number;
-}
+// jest doesn't pick up hardhat-config.ts, so we need to import the type extensions here
+import 'hardhat-switch-network/src/type-extensions';
 
 interface SetBalanceInput {
   address: string;
@@ -13,79 +9,45 @@ interface SetBalanceInput {
 }
 
 interface ForkChainResult {
-  startFork: (options?: ForkChainInput) => Promise<void>;
   setBalance: (options: SetBalanceInput) => Promise<void>;
   impersonateAccount: (address: string) => Promise<void>;
   provider: EthereumProvider;
-  setupFork: (options: {
-    fork?: ForkChainInput;
-    accounts?: SetBalanceInput[];
-  }) => Promise<void>;
+  setupFork: (options: { accounts?: SetBalanceInput[] }) => Promise<void>;
+  switchNetwork: (networkName: string) => Promise<void>;
 }
 
-export function forkChain(): ForkChainResult {
-  const startFork: ForkChainResult['startFork'] = async ({
-    rpcUrl = process.env.PROVIDER_URL!,
-    blockNumber,
-    chainId,
-  } = {}) => {
-    await hre.network.provider.request({
-      // forks chain
-      method: 'hardhat_reset',
-      params: [
-        {
-          forking: {
-            jsonRpcUrl: rpcUrl,
-            // Optionally, specify the block number
-            // blockNumber: 12345678,
-            blockNumber,
-            chainId,
-          },
-          // chainId: 1, // Set to Ethereum Mainnet's chainId
-          chainId,
-        },
-      ],
-    });
-  };
+export const HardhatProvider = hre.network.provider;
 
-  const impersonateAccount: ForkChainResult['impersonateAccount'] = async (
-    address
-  ) => {
-    await hre.network.provider.request({
-      method: 'hardhat_impersonateAccount',
-      params: [address],
-    });
-  };
+export const impersonateAccount: ForkChainResult['impersonateAccount'] = async (
+  address
+) => {
+  await hre.network.provider.request({
+    method: 'hardhat_impersonateAccount',
+    params: [address],
+  });
+};
 
-  const setBalance: ForkChainResult['setBalance'] = async ({
+export const setBalance: ForkChainResult['setBalance'] = async ({
+  address,
+  balance,
+}) => {
+  await hre.network.provider.send('hardhat_setBalance', [
     address,
-    balance,
-  }) => {
-    await hre.network.provider.send('hardhat_setBalance', [
-      address,
-      `0x${balance.toString(16)}`,
-    ]);
-  };
+    `0x${balance.toString(16)}`,
+  ]);
+};
 
-  const setupFork: ForkChainResult['setupFork'] = async ({
-    fork,
-    accounts,
-  }) => {
-    await startFork(fork);
-
-    if (accounts) {
-      for (const accountSetup of accounts) {
-        await setBalance(accountSetup);
-        await impersonateAccount(accountSetup.address);
-      }
+export const setupFork: ForkChainResult['setupFork'] = async ({ accounts }) => {
+  if (accounts) {
+    for (const accountSetup of accounts) {
+      await setBalance(accountSetup);
+      await impersonateAccount(accountSetup.address);
     }
-  };
+  }
+};
 
-  return {
-    startFork,
-    provider: hre.network.provider,
-    setBalance,
-    impersonateAccount,
-    setupFork,
-  };
-}
+export const switchNetwork: ForkChainResult['switchNetwork'] = async (
+  networkName
+) => {
+  await hre.switchNetwork(networkName);
+};
