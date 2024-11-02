98b9e7ce6289bb7e2b8a9469949895ca3a4b46ed
Velenir <velenir@gmail.com>
2024-11-02 14:51:50 +0100

    hardhat config/fixed accounts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,6 +4,9 @@ import 'hardhat-switch-network';
 
 dotenv.config();
 
+const TEST_MNEMONIC =
+  'radar blur cabbage chef fix engine embark joy scheme fiction master release';
+
 const config: HardhatUserConfig = {
   solidity: '0.8.24',
   networks: {
@@ -14,6 +17,18 @@ const config: HardhatUserConfig = {
         // blockNumber: 12345678,
       },
       chainId: 1,
+      // have to config hardhat with fixed accounts unlocked, otherwise signTx and signTyped data fail when used in RPC calls to Node
+      // which breaks web3.js
+      // ethers signs data and txs locally off-chain as long as it has provate key
+      // web3 is a bit harder to init wallets for locally.
+      // impersonateAccounts doesn't work, even though it should locgically fully unlock accounts,
+      // but only remore accounts are unlocked, fixed are not,
+      // so we always need to only use accounts generated from this mnemonic
+
+      // refs:
+      // https://github.com/NomicFoundation/hardhat/issues/3059
+      // https://github.com/NomicFoundation/hardhat/issues/1226
+      accounts: { mnemonic: TEST_MNEMONIC }, // and with fixed
       // dynamically switch between networks configured here
       // by calling `hre.switchNetwork(networkName)` thanks to hardhat-switch-network plugin
       gasPrice: 8e9,
