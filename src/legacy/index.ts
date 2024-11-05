a522f47c982dbb6a7b00a96adc4f72079a7b1966
Velenir <velenir@gmail.com>
2024-11-05 14:59:08 +0100

    legacy/support ethersV6

--- src/legacy/index.ts
+++ src/legacy/index.ts
@@ -1,5 +1,5 @@
 import type Web3 from 'web3';
-import type { ContractTransaction } from '@ethersproject/contracts';
+import type { ContractTransaction as EthersV5ContractTransaction } from '@ethersproject/contracts';
 
 import { API_URL, DEFAULT_VERSION, SwapSide } from '../constants';
 
@@ -8,7 +8,8 @@ import { assert } from 'ts-essentials';
 import {
   constructAxiosFetcher,
   constructFetchFetcher,
-  constructEthersContractCaller,
+  constructEthersV5ContractCaller,
+  constructEthersV6ContractCaller,
   constructWeb3ContractCaller,
   isFetcherError,
   Web3UnpromiEvent,
@@ -38,6 +39,7 @@ import { constructPartialSDK } from '../sdk/partial';
 import { constructGetTokens } from '../methods/swap/token';
 import { constructGetSpender } from '../methods/swap/spender';
 import { constructGetAdapters } from '../methods/swap/adapters';
+import { ContractTransactionResponse as EthersV6ContractTransactionResponse } from 'ethersV6';
 
 export type APIError = {
   message: string;
@@ -46,7 +48,10 @@ export type APIError = {
 };
 type Fetch = typeof fetch;
 
-type TxResponse = Web3UnpromiEvent | ContractTransaction;
+type TxResponse =
+  | Web3UnpromiEvent
+  | EthersV5ContractTransaction
+  | EthersV6ContractTransactionResponse;
 
 type LegacyOptions = {
   chainId?: number;
@@ -114,7 +119,9 @@ export class ParaSwap {
     }
 
     const contractCaller = ethersDeps
-      ? constructEthersContractCaller(ethersDeps, account)
+      ? 'ethersV6ProviderOrSigner' in ethersDeps
+        ? constructEthersV6ContractCaller(ethersDeps, account)
+        : constructEthersV5ContractCaller(ethersDeps, account)
       : web3Provider
       ? constructWeb3ContractCaller(web3Provider, account)
       : null;
@@ -183,16 +190,33 @@ export class ParaSwap {
   }
 
   setEthersProvider(ethersDeps: EthersProviderDeps, account?: string): this {
-    const contractCaller = constructEthersContractCaller(ethersDeps, account);
     const { apiURL, chainId, fetcher } = this;
 
-    this.sdk = constructSwapSDK({
-      fetcher,
-      contractCaller,
-      apiURL,
-      version: this.version,
-      chainId,
-    });
+    if ('ethersV6ProviderOrSigner' in ethersDeps) {
+      const contractCaller = constructEthersV6ContractCaller(
+        ethersDeps,
+        account
+      );
+      this.sdk = constructSwapSDK({
+        fetcher,
+        contractCaller,
+        apiURL,
+        version: this.version,
+        chainId,
+      });
+    } else {
+      const contractCaller = constructEthersV5ContractCaller(
+        ethersDeps,
+        account
+      );
+      this.sdk = constructSwapSDK({
+        fetcher,
+        contractCaller,
+        apiURL,
+        version: this.version,
+        chainId,
+      });
+    }
 
     this.web3Provider = undefined;
     this.ethersDeps = ethersDeps;
