e94a5e12a1b4cbb907cd9618bb7b6ea947fe1d13
Velenir <velenir@gmail.com>
2024-11-05 15:00:44 +0100

    examples/ add ethersV6

---
+++ src/examples/ethersV6.ts
@@ -0,0 +1,38 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import axios from 'axios';
+import { ethers } from 'ethersV6';
+import {
+  constructPartialSDK,
+  constructFullSDK,
+  constructGetAdapters,
+  constructEthersV6ContractCaller,
+  constructAxiosFetcher,
+} from '..';
+
+const fetcher = constructAxiosFetcher(axios);
+
+const provider = ethers.getDefaultProvider(1);
+const contractCaller = constructEthersV6ContractCaller({
+  ethersV6ProviderOrSigner: provider,
+  EthersV6Contract: ethers.Contract,
+});
+
+const paraswap = constructFullSDK({
+  chainId: 1,
+  fetcher,
+  contractCaller,
+});
+
+const res = paraswap.swap.getAdapters();
+
+// type Promise<ContractTransaction>
+const txResponse = paraswap.swap.approveToken('1', '0x...');
+// type Promise<ContractTransaction[]>
+const txResponses = paraswap.swap.approveTokenBulk('1', ['0x...']);
+
+const partial = constructPartialSDK(
+  { apiURL: '', chainId: 1, fetcher },
+  constructGetAdapters
+);
+
+const res1 = partial.getAdapters();
