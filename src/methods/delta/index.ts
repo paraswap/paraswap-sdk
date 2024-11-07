4e990e6118e3265270455239d1eacadec3b58df8
Velenir <velenir@gmail.com>
2024-11-07 19:44:59 +0100

    delta/submitOrder & allHandlers

---
+++ src/methods/delta/index.ts
@@ -0,0 +1,127 @@
+import type { ConstructProviderFetchInput } from '../../types';
+import type { DeltaAuctionOrder, ParaswapDeltaAuction } from './helpers/types';
+import {
+  BuildDeltaOrderDataParams,
+  BuildDeltaOrderFunctions,
+  constructBuildDeltaOrder,
+} from './buildDeltaOrder';
+import {
+  constructPostDeltaOrder,
+  PostDeltaOrderFunctions,
+} from './postDeltaOrder';
+import {
+  constructSignDeltaOrder,
+  SignDeltaOrderFunctions,
+} from './signDeltaOrder';
+import {
+  GetDeltaContractFunctions,
+  constructGetDeltaContract,
+} from './getDeltaContract';
+import {
+  constructGetDeltaPrice,
+  GetDeltaPriceFunctions,
+} from './getDeltaPrice';
+import {
+  constructGetDeltaOrders,
+  GetDeltaOrdersFunctions,
+} from './getDeltaOrders';
+import {
+  constructGetPartnerFee,
+  GetPartnerFeeFunctions,
+} from './getPartnerFee';
+import {
+  ApproveTokenForDeltaFunctions,
+  constructApproveTokenForDelta,
+} from './approveForDelta';
+
+type SubmitDeltaOrder = (
+  orderParams: BuildDeltaOrderDataParams
+) => Promise<ParaswapDeltaAuction>;
+
+export type SubmitDeltaOrderFuncs = {
+  submitDeltaOrder: SubmitDeltaOrder;
+};
+
+export const constructSubmitDeltaOrder = (
+  options: ConstructProviderFetchInput<any, 'signTypedDataCall'>
+): SubmitDeltaOrderFuncs => {
+  const { buildDeltaOrder } = constructBuildDeltaOrder(options);
+  const { signDeltaOrder } = constructSignDeltaOrder(options);
+  const { postDeltaOrder } = constructPostDeltaOrder(options);
+
+  const submitDeltaOrder: SubmitDeltaOrder = async (orderParams) => {
+    const orderData = await buildDeltaOrder(orderParams);
+    const signature = await signDeltaOrder(orderData);
+
+    const postOrderData: DeltaAuctionOrder = {
+      owner: orderData.data.owner,
+      beneficiary: orderData.data.beneficiary,
+      srcToken: orderData.data.srcToken,
+      destToken: orderData.data.destToken,
+      srcAmount: orderData.data.srcAmount,
+      destAmount: orderData.data.destAmount,
+      expectedDestAmount: orderData.data.expectedDestAmount,
+      deadline: orderData.data.deadline,
+      nonce: orderData.data.nonce,
+      permit: orderData.data.permit,
+      partnerAndFee: orderData.data.partnerAndFee,
+    };
+
+    const response = await postDeltaOrder({
+      signature,
+      partner: orderParams.partner,
+      order: orderData.data,
+    });
+
+    return response;
+  };
+
+  return { submitDeltaOrder };
+};
+
+export type DeltaOrderHandlers<T> = SubmitDeltaOrderFuncs &
+  ApproveTokenForDeltaFunctions<T> &
+  BuildDeltaOrderFunctions &
+  GetDeltaOrdersFunctions &
+  GetDeltaPriceFunctions &
+  GetDeltaContractFunctions &
+  GetPartnerFeeFunctions &
+  PostDeltaOrderFunctions &
+  SignDeltaOrderFunctions;
+
+/** @description construct SDK with every Delta Order-related method, fetching from API and Order signing */
+export const constructDeltaOrdersHandlers = <TxResponse>(
+  options: ConstructProviderFetchInput<
+    TxResponse,
+    'signTypedDataCall' | 'transactCall'
+  >
+): DeltaOrderHandlers<TxResponse> => {
+  const deltaOrdersGetters = constructGetDeltaOrders(options);
+  const deltaOrdersContractGetter = constructGetDeltaContract(options);
+  const deltaPrice = constructGetDeltaPrice(options);
+
+  const partnerFee = constructGetPartnerFee(options);
+
+  const approveTokenForDelta = constructApproveTokenForDelta(options);
+
+  const deltaOrdersSubmit = constructSubmitDeltaOrder(options);
+
+  const deltaOrdersBuild = constructBuildDeltaOrder(options);
+  const deltaOrdersSign = constructSignDeltaOrder(options);
+  const deltaOrdersPost = constructPostDeltaOrder(options);
+
+  // const DeltaOrdersApproveToken = constructApproveTokenForDeltaOrder(options);
+
+  return {
+    ...deltaOrdersGetters,
+    ...deltaOrdersContractGetter,
+    ...deltaPrice,
+    ...partnerFee,
+    ...approveTokenForDelta,
+    ...deltaOrdersSubmit,
+    ...deltaOrdersBuild,
+    ...deltaOrdersSign,
+    ...deltaOrdersPost,
+    // ...deltaOrdersApproveToken,
+  };
+};
