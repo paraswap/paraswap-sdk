05946bc01ede0cfeaad59382caa63d0a9ef249f5
Velenir <velenir@gmail.com>
2024-11-07 19:43:53 +0100

    delta/signOrder

---
+++ src/methods/delta/signDeltaOrder.ts
@@ -0,0 +1,37 @@
+import type { ConstructProviderFetchInput } from '../../types';
+import { SignableDeltaOrderData } from './helpers/buildDeltaOrderData';
+import { sanitizeDeltaOrderData } from './helpers/misc';
+
+export type SignLimitOrderFunctions = {
+  signLimitOrder: (
+    signableOrderData: SignableDeltaOrderData
+  ) => Promise<string>;
+};
+
+type SignDeltaOrder = (
+  signableOrderData: SignableDeltaOrderData
+) => Promise<string>;
+
+export type SignDeltaOrderFunctions = {
+  signDeltaOrder: SignDeltaOrder;
+};
+
+// returns whatever `contractCaller` returns
+// to allow for better versatility
+export const constructSignDeltaOrder = (
+  options: Pick<
+    ConstructProviderFetchInput<any, 'signTypedDataCall'>,
+    'contractCaller'
+  >
+): SignDeltaOrderFunctions => {
+  const signDeltaOrder: SignDeltaOrder = (typedData) => {
+    // types allow to pass OrderData & extra_stuff, but tx will break like that
+    const typedDataOnly: SignableDeltaOrderData = {
+      ...typedData,
+      data: sanitizeDeltaOrderData(typedData.data),
+    };
+    return options.contractCaller.signTypedDataCall(typedDataOnly);
+  };
+
+  return { signDeltaOrder };
+};
