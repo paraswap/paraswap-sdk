4c2425628df0c08ce18318659039a99e19ab8e6c
Velenir <velenir@gmail.com>
2024-11-07 19:44:22 +0100

    delta/postOrder

---
+++ src/methods/delta/postDeltaOrder.ts
@@ -0,0 +1,44 @@
+import { API_URL } from '../../constants';
+import type { ConstructFetchInput } from '../../types';
+import { DeltaAuctionOrder, ParaswapDeltaAuction } from './helpers/types';
+
+export type DeltaOrderToPost = {
+  partner?: string; // @TODO not sure if this is optional, need to check
+  order: DeltaAuctionOrder;
+  signature: string;
+  chainId: number;
+};
+
+export type PostDeltaOrderParams = Omit<DeltaOrderToPost, 'chainId'>;
+
+type DeltaOrderApiResponse = ParaswapDeltaAuction;
+
+type PostDeltaOrder = (
+  postData: PostDeltaOrderParams,
+  signal?: AbortSignal
+) => Promise<DeltaOrderApiResponse>;
+
+export type PostDeltaOrderFunctions = {
+  postDeltaOrder: PostDeltaOrder;
+};
+
+export const constructPostDeltaOrder = ({
+  apiURL = API_URL,
+  chainId,
+  fetcher,
+}: ConstructFetchInput): PostDeltaOrderFunctions => {
+  const postOrderUrl = `${apiURL}/delta/orders` as const;
+
+  const postDeltaOrder: PostDeltaOrder = (postData, signal) => {
+    const deltaOrderToPost: DeltaOrderToPost = { ...postData, chainId };
+
+    return fetcher<DeltaOrderApiResponse>({
+      url: postOrderUrl,
+      method: 'POST',
+      data: deltaOrderToPost,
+      signal,
+    });
+  };
+
+  return { postDeltaOrder };
+};
