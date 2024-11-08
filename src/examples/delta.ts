ce9a82a6179dcdfbb36a649158abeba4e86822fc
Velenir <velenir@gmail.com>
2024-11-08 13:04:58 +0100

    examples/delta/manualDeltaFlow

--- src/examples/delta.ts
+++ src/examples/delta.ts
@@ -33,7 +33,7 @@ const deltaSDK = constructPartialSDK(
 const DAI_TOKEN = '0x6b175474e89094c44da98b954eedeac495271d0f';
 const PSP_TOKEN = '0xcafe001067cdef266afb7eb5a286dcfd277f3de5';
 
-async function deltaFlow() {
+async function simpleDeltaFlow() {
   const amount = '1000000000000'; // wei
 
   const deltaPrice = await deltaSDK.getDeltaPrice({
@@ -43,6 +43,7 @@ async function deltaFlow() {
     userAddress: account,
     srcDecimals: 18,
     destDecimals: 18,
+    // partner: "..." // if available
   });
 
   const DeltaContract = await deltaSDK.getDeltaContract();
@@ -59,10 +60,61 @@ async function deltaFlow() {
   const deltaAuction = await deltaSDK.submitDeltaOrder({
     deltaPrice,
     owner: account,
+    // beneficiary: anotherAccount, // if need to send destToken to another account
+    // permit: "0x1234...", // if signed a Permit1 or Permit2 TransferFrom for DeltaContract
     srcToken: DAI_TOKEN,
     destToken: PSP_TOKEN,
     srcAmount: amount,
-    destAmount: destAmountAfterSlippage,
+    destAmount: destAmountAfterSlippage, // minimum acceptable destAmount
+  });
+
+  // poll if necessary
+  const auction = await deltaSDK.getDeltaOrderById(deltaAuction.id);
+  if (auction?.status === 'EXECUTED') {
+    console.log('Auction was executed');
+  }
+}
+async function manualDeltaFlow() {
+  const amount = '1000000000000'; // wei
+
+  const deltaPrice = await deltaSDK.getDeltaPrice({
+    srcToken: DAI_TOKEN,
+    destToken: PSP_TOKEN,
+    amount: '1000000000000',
+    userAddress: account,
+    srcDecimals: 18,
+    destDecimals: 18,
+    // partner: "..." // if available
+  });
+
+  const DeltaContract = await deltaSDK.getDeltaContract();
+
+  // or sign a Permit1 or Permit2 TransferFrom for DeltaContract
+  await deltaSDK.approveTokenForDelta(amount, DAI_TOKEN);
+
+  const slippagePercent = 0.5;
+  const destAmountAfterSlippage = (
+    +deltaPrice.destAmount *
+    (1 - slippagePercent / 100)
+  ).toString(10);
+
+  const signableOrderData = await deltaSDK.buildDeltaOrder({
+    deltaPrice,
+    owner: account,
+    // beneficiary: anotherAccount, // if need to send destToken to another account
+    // permit: "0x1234...", // if signed a Permit1 or Permit2 TransferFrom for DeltaContract
+    srcToken: DAI_TOKEN,
+    destToken: PSP_TOKEN,
+    srcAmount: amount,
+    destAmount: destAmountAfterSlippage, // minimum acceptable destAmount
+  });
+
+  const signature = await deltaSDK.signDeltaOrder(signableOrderData);
+
+  const deltaAuction = await deltaSDK.postDeltaOrder({
+    // partner: "..." // if available
+    order: signableOrderData.data,
+    signature,
   });
 
   // poll if necessary
