09a8d545945241dd844a04cda7f92d3d7abff142
Velenir <Velenir@users.noreply.github.com>
2025-04-28 19:09:31 +0300

    Fix/compacting signatures (#193)
    * signDeltaOrder/disable compact sig
    
    * fix test/update snapshot

--- src/methods/delta/signDeltaOrder.ts
+++ src/methods/delta/signDeltaOrder.ts
@@ -1,4 +1,4 @@
-import { deriveCompactSignature } from '../../helpers/misc';
+// import { deriveCompactSignature } from '../../helpers/misc';
 import type { ConstructProviderFetchInput } from '../../types';
 import { SignableDeltaOrderData } from './helpers/buildDeltaOrderData';
 import { sanitizeDeltaOrderData } from './helpers/misc';
@@ -35,19 +35,24 @@ export const constructSignDeltaOrder = (
       typedDataOnly
     );
 
-    if (signature.length > 132) {
-      // signature more than 65 bytes, likely a multisig
-      // not compatible with EIP-2098 Compact Signatures
-      return signature;
-    }
+    // Safe signature with only one signer has length 132
+    // but if it is compacted, the recovered address doesn't match the signer
 
-    // both full and compact signatures work in the ParaswapDelta contract;
-    // compact signature can be marginally more gas efficient
-    try {
-      return deriveCompactSignature(signature);
-    } catch {
-      return signature;
-    }
+    return signature;
+
+    // if (signature.length > 132) {
+    //   // signature more than 65 bytes, likely a multisig
+    //   // not compatible with EIP-2098 Compact Signatures
+    //   return signature;
+    // }
+
+    // // both full and compact signatures work in the ParaswapDelta contract;
+    // // compact signature can be marginally more gas efficient
+    // try {
+    //   return deriveCompactSignature(signature);
+    // } catch {
+    //   return signature;
+    // }
   };
 
   return { signDeltaOrder };
