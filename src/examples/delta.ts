fd507542eca38d8cce19359eca5471db64df60de
Andriy-Shymkiv <winamondezik@gmail.com>
2025-05-09 11:33:37 +0300

    delta/extended order polling expample

--- src/examples/delta.ts
+++ src/examples/delta.ts
@@ -6,6 +6,7 @@ import {
   constructEthersContractCaller,
   constructAxiosFetcher,
   constructAllDeltaOrdersHandlers,
+  DeltaAuction,
 } from '..';
 
 const fetcher = constructAxiosFetcher(axios);
@@ -70,10 +71,37 @@ async function simpleDeltaFlow() {
   });
 
   // poll if necessary
-  const auction = await deltaSDK.getDeltaOrderById(deltaAuction.id);
-  if (auction?.status === 'EXECUTED') {
-    console.log('Auction was executed');
+  function isExecutedDeltaAuction(
+    auction: Omit<DeltaAuction, 'signature'>,
+    waitForCrosschain = true // only consider executed when destChain work is done
+  ) {
+    if (auction.status !== 'EXECUTED') return false;
+
+    // crosschain Order is executed on destChain if bridgeStatus is filled
+    if (waitForCrosschain && auction.order.bridge.destinationChainId !== 0) {
+      return auction.bridgeStatus === 'filled';
+    }
+
+    return true;
   }
+
+  async function fetchOrderPeriodically(auctionId: string) {
+    const intervalId = setInterval(async () => {
+      const auction = await deltaSDK.getDeltaOrderById(auctionId);
+      console.log('checks: ', auction); // Handle or log the fetched auction as needed
+
+      if (isExecutedDeltaAuction(auction)) {
+        clearInterval(intervalId); // Stop interval if completed
+        console.log('Order completed');
+      }
+    }, 3000);
+    console.log('Order Pending');
+    // Return intervalId to enable clearing the interval if needed externally
+    return intervalId;
+  }
+
+  const intervalId = await fetchOrderPeriodically(deltaAuction.id);
+  setTimeout(() => clearInterval(intervalId), 60000); // Stop after 60 seconds
 }
 async function manualDeltaFlow() {
   const amount = '1000000000000'; // wei
